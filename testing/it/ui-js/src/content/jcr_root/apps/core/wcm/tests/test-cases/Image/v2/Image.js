869fe2a76ff51b895955ec8c0f8b24f7deded1b5
Richard Hand <pid90611@adobe.com>
2018-05-23 22:45:22 +0200

    [Image] Image map support for image component
    - adds ui-js test case

--- testing/it/ui-js/src/content/jcr_root/apps/core/wcm/tests/test-cases/Image/v2/Image.js
+++ testing/it/ui-js/src/content/jcr_root/apps/core/wcm/tests/test-cases/Image/v2/Image.js
@@ -27,6 +27,15 @@ window.CQ.CoreComponentsIT.Image.v2 = window.CQ.CoreComponentsIT.Image.v2 || {};
     var originalDamTitle       = "Beach house";
     var originalDamDescription = "House on a beach with blue sky";
 
+    var selectors = {
+        elements: {
+            self: ".cmp-image",
+            image: "[data-cmp-hook-image='image']",
+            map: "[data-cmp-hook-image='map']",
+            area: "[data-cmp-hook-image='area']"
+        }
+    };
+
     image.tcDragImage = function() {
         return new h.TestCase("Drag Asset")
             .execTestCase(c.tcOpenConfigureDialog("cmpPath"))
@@ -110,6 +119,115 @@ window.CQ.CoreComponentsIT.Image.v2 = window.CQ.CoreComponentsIT.Image.v2 || {};
             .config.changeContext(c.getContentFrame)
             .asserts.isTrue(function() {
                 return h.find(".cmp-image__image").attr("alt") === "" && h.find(".cmp-image__link").size() === 0;
+            });
+    };
+
+    /**
+     * Test: Check image map areas are rendered, navigate correctly and are responsively adjusted on window resize
+     */
+    image.tcCheckMapAreaNavigationAndResponsiveResize = function(tcExecuteBeforeTest, tcExecuteAfterTest) {
+        return new h.TestCase("Check map area navigation and responsive resize", {
+            execBefore: tcExecuteBeforeTest,
+            execAfter: tcExecuteAfterTest }
+        )
+
+            // create and assign a content policy to enable lazy loading; which initializes the frontend handling
+            .execFct(function(opts, done) {
+                var componentPath = h.param("cmpPath")();
+                h.param("cmpNodeName", componentPath.substr(componentPath.lastIndexOf("/")));
+                var data = {};
+                data["jcr:title"] = "New Policy";
+                data["sling:resourceType"] = "wcm/core/components/policy/policy";
+                data["disableLazyLoading"] = "false";
+
+                c.createPolicy(h.param("cmpNodeName")() + "/new_policy", data, "policyPath", done, c.policyPath);
+            })
+
+            .execFct(function(opts, done) {
+                var data = {};
+                data["cq:policy"] = "core-component/components" + h.param("cmpNodeName")() + "/new_policy";
+                data["sling:resourceType"] = "wcm/core/components/policies/mapping";
+
+                c.assignPolicy(h.param("cmpNodeName")(), data, done, c.policyAssignmentPath);
+            })
+
+            // set up an example image and persist a test image map with a single map area
+            .execTestCase(image.tcDragImage())
+            .execTestCase(c.tcSaveConfigureDialog)
+            .wait(500)
+            .execFct(function(opts, done) {
+                var data = {};
+                data.imageMap = "[rect(0,0,226,230)\"/content/we-retail/us/en\"|\"\"|\"Alt Text\"|(0.0000,0.0000,0.1948,0.2295)]";
+
+                c.editNodeProperties(h.param("cmpPath")(), data, done);
+            })
+            .wait(500)
+
+            // refresh the component rendering
+            .execTestCase(c.tcOpenConfigureDialog("cmpPath"))
+            .execTestCase(c.tcSaveConfigureDialog)
+
+            // verify the map area is available
+            .asserts.isTrue(function() {
+                return h.find(selectors.elements.map + '[name*="' + h.param("testPagePath")() +
+                    '/jcr:content/root/responsivegrid/image"] ' + selectors.elements.area, "#ContentFrame").size() === 1;
+            })
+            .wait(500)
+
+            // switch to the content frame, click the area link and verify navigation
+            .config.changeContext(c.getContentFrame)
+            .click(selectors.elements.area + ":first-child")
+            .asserts.isTrue(function() {
+                var pathname = h.context().window.location.pathname;
+                return pathname.includes("/content/we-retail/us/en");
+            })
+
+            // navigate back to the test page
+            .config.resetContext()
+            .navigateTo("/editor.html%testPagePath%.html")
+            .config.changeContext(c.getContentFrame)
+
+            // manually resize the test image to a narrow width and trigger a window resize
+            .execFct(function() {
+                h.find(selectors.elements.image).width(100);
+                $(h.context().window).trigger("resize");
+            })
+            .wait(500)
+
+            // verify that the applied coordinates are correct in relation to the new image size
+            .asserts.isTrue(function() {
+                var area = h.find(selectors.elements.area)[0];
+                var image = h.find(selectors.elements.image)[0];
+                var coords = area.coords.split(",");
+                var relativeCoords = area.dataset.cmpRelcoords.split(",");
+                var width = image.width;
+                var height = image.height;
+                var passed = true;
+
+                var expectedCoords = [
+                    Math.floor((relativeCoords[0] * width)),
+                    Math.floor((relativeCoords[1] * height)),
+                    Math.floor((relativeCoords[2] * width)),
+                    Math.floor((relativeCoords[3] * height))
+                ];
+
+                for (var i = 0; i < coords.length; i++) {
+                    if (parseInt(coords[i]) !== expectedCoords[i]) {
+                        passed = false;
+                        break;
+                    }
+                }
+
+                return passed;
+            })
+
+            // cleanup
+            .execFct(function(opts, done) {
+                c.deletePolicy(h.param("cmpNodeName")(), done, c.policyPath);
+            })
+
+            .execFct(function(opts, done) {
+                c.deletePolicyAssignment(h.param("cmpNodeName")(), done, c.policyAssignmentPath);
             });
     };
 
