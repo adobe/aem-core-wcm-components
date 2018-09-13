1c08613794cbaef2f9636aa66cc1828d5b3b5adc
Philip Mundt <mundt@adobe.com>
2018-09-13 16:08:05 +0200

    Extract logic from ContentFragmentImpl to Utils
    In order to re-use common cfm logic we extract it to a utility class
    and provide additional tests.

--- extension/contentfragment/bundle/src/main/java/com/adobe/cq/wcm/core/components/extension/contentfragment/internal/models/v1/ContentFragmentImpl.java
+++ extension/contentfragment/bundle/src/main/java/com/adobe/cq/wcm/core/components/extension/contentfragment/internal/models/v1/ContentFragmentImpl.java
@@ -19,22 +19,17 @@ import com.adobe.cq.dam.cfm.ContentElement;
 import com.adobe.cq.dam.cfm.ContentFragmentException;
 import com.adobe.cq.dam.cfm.ContentVariation;
 import com.adobe.cq.dam.cfm.FragmentData;
-import com.adobe.cq.dam.cfm.FragmentTemplate;
 import com.adobe.cq.dam.cfm.content.FragmentRenderService;
 import com.adobe.cq.dam.cfm.converter.ContentTypeConverter;
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.wcm.core.components.extension.contentfragment.internal.ContentFragmentUtils;
 import com.adobe.cq.wcm.core.components.extension.contentfragment.models.ContentFragment;
-import com.day.cq.wcm.api.policies.ContentPolicy;
-import com.day.cq.wcm.api.policies.ContentPolicyManager;
-import com.day.text.Text;
 import org.apache.commons.collections.IteratorUtils;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
@@ -48,20 +43,14 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import static com.adobe.cq.wcm.core.components.extension.contentfragment.internal.models.v1.ContentFragmentImpl.RESOURCE_TYPE;
-import static com.day.cq.commons.jcr.JcrConstants.JCR_CONTENT;
-import static com.day.cq.commons.jcr.JcrConstants.JCR_TITLE;
 import static org.apache.sling.models.annotations.injectorspecific.InjectionStrategy.OPTIONAL;
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {ContentFragment.class, ComponentExporter.class}, resourceType = RESOURCE_TYPE)
@@ -74,8 +63,6 @@ public class ContentFragmentImpl implements ContentFragment {
      * The resource type of the component associated with this Sling model.
      */
     public static final String RESOURCE_TYPE = "core/wcm/extension/components/contentfragment/v1/contentfragment";
-
-    private static final String DEFAULT_GRID_TYPE = "dam/cfm/components/grid";
 
     /**
      * The name of the master variation.
@@ -136,26 +123,12 @@ public class ContentFragmentImpl implements ContentFragment {
     }
 
     private void initializeElements() {
-        // get either all elements...
-        Iterator<ContentElement> iterator = fragment.getElements();
-        // ...or configured elements
-        if (ArrayUtils.isNotEmpty(elementNames)) {
-            List<ContentElement> elements = new LinkedList<>();
-            for (String name : elementNames) {
-                if (!fragment.hasElement(name)) {
-                    // skip non-existing element
-                    LOG.warn("Skipping non-existing element " + name);
-                    continue;
-                }
-                elements.add(fragment.getElement(name));
-            }
-            iterator = elements.iterator();
-        }
+        Iterator<ContentElement> contentElementIterator = ContentFragmentUtils.filterElements(fragment, elementNames);
 
         // wrap elements and get their configured variation (if any)
         exportedElements = new LinkedHashMap<>();
-        while (iterator.hasNext()) {
-            ContentElement element = iterator.next();
+        while (contentElementIterator.hasNext()) {
+            ContentElement element = contentElementIterator.next();
             ContentVariation variation = null;
             if (StringUtils.isNotEmpty(variationName) && !MASTER_VARIATION.equals(variationName)) {
                 variation = element.getVariation(variationName);
@@ -163,9 +136,7 @@ public class ContentFragmentImpl implements ContentFragment {
                     LOG.warn("Non-existing variation " + variationName + " of element " + element.getName());
                 }
             }
-            exportedElements.put(
-                    element.getName(),
-                    new ElementImpl(renderService, converter, resource, element, variation));
+            exportedElements.put(element.getName(), new ElementImpl(renderService, converter, resource, element, variation));
         }
 
         elements = new ArrayList<>(exportedElements.values());
@@ -194,36 +165,7 @@ public class ContentFragmentImpl implements ContentFragment {
     @Override
     public String getType() {
         if (type == null && fragment != null) {
-            Resource fragmentResource = fragment.adaptTo(Resource.class);
-            FragmentTemplate template = fragment.getTemplate();
-            Resource templateResource = template.adaptTo(Resource.class);
-            if (fragmentResource == null || templateResource == null) {
-                LOG.warn("Unable to return type: fragment or template resource is null");
-                type = fragment.getName();
-            } else {
-                // use the parent if the template resource is the jcr:content child
-                Resource parent = templateResource.getParent();
-                if (JCR_CONTENT.equals(templateResource.getName()) && parent != null) {
-                    templateResource = parent;
-                }
-                // get data node to check if this is a text-only or structured content fragment
-                Resource data = fragmentResource.getChild(JCR_CONTENT + "/data");
-                if (data == null || data.getValueMap().get("cq:model") == null) {
-                    // this is a text-only content fragment, for which we use the model path as the type
-                    type = templateResource.getPath();
-                } else {
-                    // this is a structured content fragment, assemble type string (e.g. "my-project/models/my-model" or
-                    // "my-project/nested/models/my-model")
-                    StringBuilder prefix = new StringBuilder();
-                    String[] segments = Text.explode(templateResource.getPath(), '/', false);
-                    // get the configuration names (e.g. for "my-project/" or "my-project/nested/")
-                    for (int i = 1; i < segments.length - 5; i++) {
-                        prefix.append(segments[i]);
-                        prefix.append("/");
-                    }
-                    type = prefix.toString() + "models/" + templateResource.getName();
-                }
-            }
+            type = ContentFragmentUtils.getType(fragment);
         }
         return type;
     }
@@ -231,48 +173,13 @@ public class ContentFragmentImpl implements ContentFragment {
     @Nonnull
     @Override
     public String getGridResourceType() {
-        String gridResourceType = DEFAULT_GRID_TYPE;
-        ContentPolicyManager policyManager = resolver.adaptTo(ContentPolicyManager.class);
-        ContentPolicy fragmentPolicy = policyManager != null ? policyManager.getPolicy(resource) : null;
-        if (fragmentPolicy != null) {
-            ValueMap props = fragmentPolicy.getProperties();
-            gridResourceType = props.get("cfm-grid-type", DEFAULT_GRID_TYPE);
-        }
-        return gridResourceType;
+        return ContentFragmentUtils.getGridResourceType(resolver, resource);
     }
 
     @Nonnull
     @Override
     public String getEditorJSON() {
-        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
-        jsonObjectBuilder.add("title", fragment.getTitle());
-        jsonObjectBuilder.add("path", path);
-        if (variationName != null) {
-            jsonObjectBuilder.add("variation", variationName);
-        }
-        if (elementNames != null) {
-            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-            for (String ele : elementNames) {
-                arrayBuilder.add(ele);
-            }
-            jsonObjectBuilder.add("elements", arrayBuilder);
-        }
-        Iterator<Resource> associatedContentIter = fragment.getAssociatedContent();
-        if (associatedContentIter.hasNext()) {
-            JsonArrayBuilder associatedContentArray = Json.createArrayBuilder();
-            while (associatedContentIter.hasNext()) {
-                Resource resource = associatedContentIter.next();
-                ValueMap vm = resource.adaptTo(ValueMap.class);
-                JsonObjectBuilder contentObject = Json.createObjectBuilder();
-                if (vm!= null && vm.containsKey(JCR_TITLE)) {
-                    contentObject.add("title", vm.get(JCR_TITLE, String.class));
-                }
-                contentObject.add("path", resource.getPath());
-                associatedContentArray.add(contentObject);
-            }
-            jsonObjectBuilder.add("associatedContent", associatedContentArray);
-        }
-        return jsonObjectBuilder.build().toString();
+        return ContentFragmentUtils.getEditorJSON(fragment, variationName, elementNames);
     }
 
     @Nonnull
@@ -287,19 +194,13 @@ public class ContentFragmentImpl implements ContentFragment {
     @Nonnull
     @Override
     public String[] getExportedElementsOrder() {
-        Map<String, ContentFragment.Element> elements = getExportedElements();
-
-        if (elements.isEmpty()) {
-            return ArrayUtils.EMPTY_STRING_ARRAY;
-        }
-
-        return elements.keySet().toArray(ArrayUtils.EMPTY_STRING_ARRAY);
+        return ContentFragmentUtils.getItemsOrder(getExportedElements());
     }
 
     @Nullable
     @Override
     public List<Element> getElements() {
-        if (!isInitialized && fragment != null)  {
+        if (!isInitialized && fragment != null) {
             initializeElements();
         }
         return elements;
@@ -323,38 +224,16 @@ public class ContentFragmentImpl implements ContentFragment {
     @Nonnull
     @Override
     public Map<String, ComponentExporter> getExportedItems() {
-        Map<String, ComponentExporter> map = new LinkedHashMap<>();
-        Iterator<Resource> children = resource.listChildren();
-
-        while (children.hasNext()) {
-            Resource child = children.next();
-            ComponentExporter exporter =
-                modelFactory.getModelFromWrappedRequest(request, child, ComponentExporter.class);
-
-            if (exporter != null) {
-                String name = child.getName();
-                if (map.put(name, exporter) != null) {
-                    throw new IllegalStateException("Duplicate key: " + name);
-                }
-            }
-
-        }
-        return map;
+        return ContentFragmentUtils.getComponentExporters(resource.listChildren(), modelFactory, request);
     }
 
     @Nonnull
     @Override
     public String[] getExportedItemsOrder() {
-        Map<String, ComponentExporter> models = getExportedItems();
-
-        if (models.isEmpty()) {
-            return ArrayUtils.EMPTY_STRING_ARRAY;
-        }
-
-        return models.keySet().toArray(ArrayUtils.EMPTY_STRING_ARRAY);
+        return ContentFragmentUtils.getItemsOrder(getExportedItems());
     }
 
-    static class ElementImpl implements Element {
+    public static class ElementImpl implements Element {
 
         private FragmentRenderService renderService;
         private ContentTypeConverter converter;
@@ -364,16 +243,15 @@ public class ContentFragmentImpl implements ContentFragment {
         private String htmlValue;
 
         /**
-         *
          * @param renderService the render service to use to render the HTML and paragraphs of text elements
-         * @param converter the converter to use to convert the value of text elements to HTML
-         * @param component the component resource
-         * @param element the original element
-         * @param variation the configured variation of the element, or {@code null}
+         * @param converter     the converter to use to convert the value of text elements to HTML
+         * @param component     the component resource
+         * @param element       the original element
+         * @param variation     the configured variation of the element, or {@code null}
          */
-        ElementImpl(@Nonnull FragmentRenderService renderService, @Nonnull ContentTypeConverter converter,
-                    @Nonnull Resource component, @Nonnull ContentElement element,
-                    @Nullable ContentVariation variation) {
+        public ElementImpl(@Nonnull FragmentRenderService renderService, @Nonnull ContentTypeConverter converter,
+                           @Nonnull Resource component, @Nonnull ContentElement element,
+                           @Nullable ContentVariation variation) {
             this.renderService = renderService;
             this.converter = converter;
             this.component = component;
@@ -448,7 +326,7 @@ public class ContentFragmentImpl implements ContentFragment {
             }
 
             String contentType = getContentType();
-            String [] values = getData().getValue(String[].class);
+            String[] values = getData().getValue(String[].class);
             String value = null;
             if (values != null) {
                 value = StringUtils.join(values, ", ");
