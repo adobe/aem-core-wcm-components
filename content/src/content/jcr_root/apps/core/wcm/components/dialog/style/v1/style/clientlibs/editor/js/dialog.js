c485924b3bd15af99863f868b28d78447d81fc04
bk <bk@headwire.com>
2018-10-05 16:53:11 -0700

    Adding licenses.

--- content/src/content/jcr_root/apps/core/wcm/components/dialog/style/v1/style/clientlibs/editor/js/dialog.js
+++ content/src/content/jcr_root/apps/core/wcm/components/dialog/style/v1/style/clientlibs/editor/js/dialog.js
@@ -1,13 +1,28 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2017 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 (function(document, $, Coral) {
     "use strict";
 
     $(document).on("click", ".cq-dialog-submit", function(e) {
-    	$(".dialog-style-system__hidden").empty();
-        $(".dialog-style-system__item input:checked").each(function(i, element){
-        	var parent = element.parentNode;
-        	var styleId = parent.id;
-			var styleIdInput = $('<input/>').attr({type: 'hidden', name: './cq:styleIds', value: styleId});
-			$(".dialog-style-system__hidden").append(styleIdInput);
+        $(".dialog-style-system__hidden").empty();
+        $(".dialog-style-system__item input:checked").each(function(i, element) {
+            var parent = element.parentNode;
+            var styleId = parent.id;
+            var styleIdInput = $("<input/>").attr({ type: "hidden", name: "./cq:styleIds", value: styleId });
+            $(".dialog-style-system__hidden").append(styleIdInput);
         });
     });
 
