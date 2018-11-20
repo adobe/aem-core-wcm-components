e8fa870ae3c2036f985b5736c116f754797a8cde
Richard Hand <pid90611@adobe.com>
2018-11-20 11:49:28 +0100

    [Tabs][Carousel][GH#298] - The Json export doesn't work properly for nested Tabs and Carousels
    - writes nested JSON output
    - writes namespaced cq:panelTitle property
    - updates panelcontainer and panelselect logic
    - updates implementations to reuse more code
    - updates test cases

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/AbstractContainerImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/AbstractContainerImpl.java
@@ -15,18 +15,29 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.wcm.core.components.internal.models.v1;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Nonnull;
+
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.factory.ModelFactory;
 
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.SlingModelFilter;
 import com.adobe.cq.wcm.core.components.models.Container;
 import com.adobe.cq.wcm.core.components.models.ListItem;
 import com.day.cq.wcm.api.components.Component;
 import com.day.cq.wcm.api.components.ComponentManager;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * Abstract class which can be used as base class for {@link Container} implementations.
@@ -39,29 +50,131 @@ public abstract class AbstractContainerImpl implements Container {
     @Self
     protected SlingHttpServletRequest request;
 
-    protected List<ListItem> items;
+    @OSGiService
+    protected SlingModelFilter slingModelFilter;
 
-    private List<ListItem> readItems() {
-        List<ListItem> items = new ArrayList<>();
+    @OSGiService
+    protected ModelFactory modelFactory;
+
+    protected List<ListItem> items;
+    protected List<Resource> childComponents;
+    protected List<Resource> filteredChildComponents;
+
+    protected Map<String, ? extends ComponentExporter> itemModels;
+    private String[] exportedItemsOrder;
+
+    /**
+     * Read the list of children resources that are components
+     *
+     * @return
+     */
+    @Nonnull
+    private List<Resource> readChildren() {
+        List<Resource> children = new LinkedList<>();
         if (resource != null) {
             ComponentManager componentManager = request.getResourceResolver().adaptTo(ComponentManager.class);
             if (componentManager != null) {
-                for (Resource res : resource.getChildren()) {
+                resource.getChildren().forEach(res -> {
                     Component component = componentManager.getComponentOfResource(res);
                     if (component != null) {
-                        items.add(new ResourceListItemImpl(request, res));
+                        children.add(res);
                     }
-                }
+                });
             }
         }
+        return children;
+    }
+
+    /**
+     * Return (and cache) the list of children resources that are components
+     *
+     * @return
+     */
+    @Nonnull
+    protected List<Resource> getChildren() {
+        if (childComponents == null) {
+            childComponents = readChildren();
+        }
+        return childComponents;
+    }
+
+    /**
+     * Return (and cache) the list of children resources that are components, filtered by the Sling Model Filter. This
+     * should only be used for JSON export, for other usages refer to {@link AbstractContainerImpl#getChildren}.
+     *
+     * @return
+     */
+    @Nonnull
+    protected List<Resource> getFilteredChildren() {
+        if (filteredChildComponents == null) {
+            filteredChildComponents = new LinkedList<>();
+            slingModelFilter.filterChildResources(getChildren())
+                .forEach(filteredChildComponents::add);
+        }
+        return filteredChildComponents;
+    }
+
+    /**
+     * Read the list of items in the container
+     *
+     * @return
+     */
+    @Nonnull
+    protected List<ListItem> readItems() {
+        List<ListItem> items = new LinkedList<>();
+        getChildren().forEach(res -> {
+            items.add(new ResourceListItemImpl(request, res));
+        });
         return items;
     }
 
     @Override
+    @JsonIgnore
     public List<ListItem> getItems() {
         if (items == null) {
             items = readItems();
         }
         return items;
+    }
+
+    @Nonnull
+    @Override
+    public String getExportedType() {
+        return resource.getResourceType();
+    }
+
+    @Nonnull
+    @Override
+    public Map<String, ? extends ComponentExporter> getExportedItems() {
+        if (itemModels == null) {
+            itemModels = getItemModels(request, ComponentExporter.class);
+        }
+        return itemModels;
+    }
+
+    @Nonnull
+    @Override
+    public String[] getExportedItemsOrder() {
+        if (exportedItemsOrder == null) {
+            Map<String, ? extends ComponentExporter> models = getExportedItems();
+            if (!models.isEmpty()) {
+                exportedItemsOrder = models.keySet().toArray(ArrayUtils.EMPTY_STRING_ARRAY);
+            } else {
+                exportedItemsOrder = ArrayUtils.EMPTY_STRING_ARRAY;
+            }
+        }
+        return Arrays.copyOf(exportedItemsOrder, exportedItemsOrder.length);
+    }
+
+    protected Map<String, ComponentExporter> getItemModels(@Nonnull SlingHttpServletRequest request,
+                                                           @Nonnull Class<ComponentExporter> modelClass) {
+        Map<String, ComponentExporter> models = new LinkedHashMap<>();
+        getFilteredChildren().forEach(child -> {
+            ComponentExporter model = modelFactory.getModelFromWrappedRequest(request, child, modelClass);
+            if (model != null) {
+                models.put(child.getName(), model);
+            }
+        });
+        return models;
     }
 }
