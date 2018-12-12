0a115ae70c5a41b676b75c9258e71389917396e7
orlowska <orlowska@adobe.com>
2018-12-12 15:25:37 +0100

    Sling export of a hierarchy of pages -- add the structure for an IT

---
+++ testing/it/ui-js/src/content/jcr_root/apps/core/wcm/tests/test-cases/Page/v3/Page.js
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright 2016 Adobe Systems Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+window.CQ.CoreComponentsIT.Page.v3 = window.CQ.CoreComponentsIT.Page.v3 || {};
+
+/**
+ * Tests for the core page component.
+ */
+(function(h, $) {
+    'use strict';
+
+    // shortcut
+    var pageV3 = window.CQ.CoreComponentsIT.Page.v3;
+
+    var configuration = '/conf/core-components';
+
+    /**
+     * Test: Check the Advanced Configuration option of a page properties.
+     */
+    pageV3.tcAdvancedConfigurationPageProperties = function(tcExecuteBeforeTest, tcExecuteAfterTest) {
+        return new h.TestCase('Check the JSON export of a hierarchy of pages', {
+            execBefore: tcExecuteBeforeTest,
+            execAfter: tcExecuteAfterTest,
+            metadata: {
+                ignoreOn63: true
+            } })
+
+
+    };
+
+}(hobs, jQuery));
