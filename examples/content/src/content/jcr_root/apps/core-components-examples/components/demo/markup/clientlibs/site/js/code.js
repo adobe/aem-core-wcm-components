c12f03ab6af39ebae324f3e12a8a7ff98a899d9b
Vlad Băilescu <bailescu@adobe.com>
2018-12-13 09:53:39 +0200

    [Library] Create Content Package and Content Structure #408 #409
    * Updated demo component JSON output

--- examples/content/src/content/jcr_root/apps/core-components-examples/components/demo/markup/clientlibs/site/js/code.js
+++ examples/content/src/content/jcr_root/apps/core-components-examples/components/demo/markup/clientlibs/site/js/code.js
@@ -24,7 +24,28 @@
         return p.innerHTML;
     }
 
+    function getJSON(url, callback) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                // Success!
+                var data = JSON.parse(request.responseText);
+                callback(data);
+            } else {
+                // We reached our target server, but it returned an error
+
+            }
+        };
+        request.send();
+    }
+
     document.addEventListener("DOMContentLoaded", function() {
+        window.top.document.querySelectorAll("pre.cmp-demo__json code").forEach(function(block) {
+            getJSON(block.dataset.cmpSrc + ".model.json", function(data) {
+                block.innerHTML = JSON.stringify(data);
+            });
+        });
         window.top.document.querySelectorAll("pre.cmp-demo__code code").forEach(function(block) {
             block.innerHTML = escapeHtml(html_beautify(block.innerHTML, { "preserve_newlines": false }));
         });
