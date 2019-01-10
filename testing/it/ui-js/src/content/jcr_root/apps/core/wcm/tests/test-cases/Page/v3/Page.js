9f1d34bd1fa23e5990f50a9432310a03ca6de15c
orlowska <orlowska@adobe.com>
2019-01-10 14:08:48 +0100

    Sling export of a hierarchy of pages -- test if it is possible to export page as JSON, and that it has all the children from the page hierarchy -- added all v1 and v2 tests to be executed against the v3 nested structure

--- testing/it/ui-js/src/content/jcr_root/apps/core/wcm/tests/test-cases/Page/v3/Page.js
+++ testing/it/ui-js/src/content/jcr_root/apps/core/wcm/tests/test-cases/Page/v3/Page.js
@@ -20,23 +20,76 @@ window.CQ.CoreComponentsIT.Page.v3 = window.CQ.CoreComponentsIT.Page.v3 || {};
  * Tests for the core page component.
  */
 (function(h, $) {
-    'use strict';
+    "use strict";
 
     // shortcut
+    var c = window.CQ.CoreComponentsIT.commons;
     var pageV3 = window.CQ.CoreComponentsIT.Page.v3;
 
-    var configuration = '/conf/core-components';
+
+    var LV1_PAGE1_PATH_PARAM = "lv1_page1_path";
+    var LV1_PAGE2_PATH_PARAM = "lv1_page2_path";
+    var LV2_PAGE1_PATH_PARAM = "lv2_page1_path";
 
     /**
-     * Test: Check the Advanced Configuration option of a page properties.
+     * Before Test Case
      */
-    pageV3.tcAdvancedConfigurationPageProperties = function(tcExecuteBeforeTest, tcExecuteAfterTest) {
-        return new h.TestCase('Check the JSON export of a hierarchy of pages', {
+    pageV3.tcExecuteBeforeTest = function() {
+        var pageRT = "core/wcm/tests/components/test-page-v3";
+        return new h.TestCase("Setup Before Test")
+            .execTestCase(c.tcExecuteBeforeTest)
+            // create a parent page
+            .execFct(function(opts, done) {
+                var rootTemplate = "/conf/core-components/settings/wcm/templates/core-components-v3-root";
+                c.createPage(rootTemplate, c.rootPage, "page_" + Date.now(), "testPagePath", done, pageRT);
+            })
+            // add 1st sub-page
+            .execFct(function(opts, done) {
+                c.createPage(c.template, h.param("testPagePath")(opts), "page1_lv1", LV1_PAGE1_PATH_PARAM, done, pageRT);
+            })
+            // add 2nd sub-page
+            .execFct(function(opts, done) {
+                c.createPage(c.template, h.param("testPagePath")(opts), "page2_lv1", LV1_PAGE2_PATH_PARAM, done, pageRT);
+            })
+            // add lv2 sub-page
+            .execFct(function(opts, done) {
+                c.createPage(c.template, h.param(LV1_PAGE1_PATH_PARAM)(opts), "page_lv2", LV2_PAGE1_PATH_PARAM, done, pageRT);
+            });
+    };
+
+    /**
+     * Test: Check the JSON representation of a hierarchy of pages.
+     *
+     * 1: get the JSON representation of the page structure
+     * 2: check if all of the children are there
+     */
+    pageV3.tcHierarchyJSON = function(tcExecuteBeforeTest, tcExecuteAfterTest) {
+        return new h.TestCase("Check the JSON export of a hierarchy of pages", {
             execBefore: tcExecuteBeforeTest,
             execAfter: tcExecuteAfterTest,
             metadata: {
                 ignoreOn63: true
             } })
+
+            // 1
+            .execFct(function(opts, done) {
+                c.getJSON(h.param("testPagePath")() + ".model.json", "modelJSON", done);
+            })
+
+            // 2
+            .assert.isTrue(function() {
+                var children = h.param("modelJSON")()[":children"];
+
+                var paths = [
+                    h.param(LV1_PAGE1_PATH_PARAM)(),
+                    h.param(LV1_PAGE2_PATH_PARAM)(),
+                    h.param(LV2_PAGE1_PATH_PARAM)()
+                ];
+
+                return (Object.keys(children).length === 3 &&
+                    children[paths[0]] && children[paths[1]] && children[paths[2]]);
+            })
+        ;
 
 
     };
