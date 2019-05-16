a3d60ea62defbf92c0b6e928065c1f5113629c34
brandonmaynard <brandon.maynard@hs2solutions.com>
2019-05-16 16:34:12 -0500

    [Accordion] Create accordion component

---
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/AccordionImpl.java
@@ -0,0 +1,57 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2019 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+package com.adobe.cq.wcm.core.components.internal.models.v1;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ContainerExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.wcm.core.components.models.Accordion;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(
+    adaptables = SlingHttpServletRequest.class,
+    adapters = { Accordion.class, ComponentExporter.class, ContainerExporter.class },
+    resourceType = AccordionImpl.RESOURCE_TYPE
+)
+@Exporter(
+    name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+    extensions = ExporterConstants.SLING_MODEL_EXTENSION
+)
+public class AccordionImpl extends PanelContainerImpl implements Accordion {
+
+    public final static String RESOURCE_TYPE = "core/wcm/components/accordion/v1/accordion";
+
+    @ValueMapValue(optional = true)
+    private String expandedItem;
+
+    private String expandedItemName;
+
+    @Override
+    public String getExpandedItem() {
+        if (expandedItemName == null) {
+            Resource expanded = resource.getChild(expandedItem);
+            if (expanded != null) {
+                expandedItemName = expandedItem;
+            }
+        }
+        return expandedItemName;
+    }
+}
---
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/Accordion.java
@@ -0,0 +1,36 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2019 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+package com.adobe.cq.wcm.core.components.models;
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+/**
+ * Defines the {@code Accordion} Sling Model used for the {@code /apps/core/wcm/components/accordion} component.
+ * @since com.adobe.cq.wcm.core.components.models 12.5.0
+ */
+@ConsumerType
+public interface Accordion extends Container {
+
+    /**
+     * Returns the expanded item
+     * @return The expanded item
+     * @since com.adobe.cq.wcm.core.components.models 12.5.0
+     */
+    default String getExpandedItem() {
+        throw new UnsupportedOperationException();
+    }
+}
