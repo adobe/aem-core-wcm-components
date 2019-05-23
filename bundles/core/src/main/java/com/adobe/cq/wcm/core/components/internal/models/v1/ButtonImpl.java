9d8266ecbff0c246b164d256e0dc7ef70c061346
Richard Hand <pid90611@adobe.com>
2019-05-23 20:31:00 +0200

    [Button] Create Button component

---
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/ButtonImpl.java
@@ -0,0 +1,74 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2019 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.wcm.core.components.internal.models.v1;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.jetbrains.annotations.NotNull;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.wcm.core.components.models.Button;
+
+@Model(
+    adaptables = SlingHttpServletRequest.class,
+    adapters = {Button.class, ComponentExporter.class},
+    resourceType = ButtonImpl.RESOURCE_TYPE
+)
+@Exporter(
+    name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+    extensions = ExporterConstants.SLING_MODEL_EXTENSION
+)
+public class ButtonImpl implements Button {
+
+    public static final String RESOURCE_TYPE = "core/wcm/components/button/v1/button";
+
+    @Self
+    private SlingHttpServletRequest request;
+
+    @ValueMapValue(optional = true)
+    private String text;
+
+    @ValueMapValue(optional = true)
+    private String link;
+
+    @ValueMapValue(optional = true)
+    private String icon;
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String getLink() {
+        return link;
+    }
+
+    @Override
+    public String getIcon() {
+        return icon;
+    }
+
+    @NotNull
+    @Override
+    public String getExportedType() {
+        return request.getResource().getResourceType();
+    }
+}
