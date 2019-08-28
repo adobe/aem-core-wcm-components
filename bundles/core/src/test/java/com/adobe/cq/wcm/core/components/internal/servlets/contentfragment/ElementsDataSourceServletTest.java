6d2c4bf92c230bc487557f4e40742c6e8e8d241d
Burkhard Pauli <pauli@adobe.com>
2019-08-28 15:46:49 +0200

    Migrate unit test to aem mocks 2.7 (#708)
    - switched to AEM Mocks 2.7.0 with Content Policy Support
    - removed custom content policy mapping from all model based tests
    - updated dependency of AEM mocks
    - migrate JUnit tests to JUnit 5
    - updated copyright header

--- bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/contentfragment/ElementsDataSourceServletTest.java
+++ bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/contentfragment/ElementsDataSourceServletTest.java
@@ -1,5 +1,5 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ~ Copyright 2017 Adobe Systems Incorporated
+ ~ Copyright 2017 Adobe
  ~
  ~ Licensed under the Apache License, Version 2.0 (the "License");
  ~ you may not use this file except in compliance with the License.
@@ -16,18 +16,17 @@
 package com.adobe.cq.wcm.core.components.internal.servlets.contentfragment;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.adobe.cq.wcm.core.components.testing.Utils;
 import com.adobe.granite.ui.components.ds.DataSource;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(AemContextExtension.class)
 public class ElementsDataSourceServletTest extends AbstractContentFragmentDataSourceServletTest {
 
     /* names of the datasource instances to test */
@@ -57,92 +56,93 @@ public class ElementsDataSourceServletTest extends AbstractContentFragmentDataSo
 
     private ElementsDataSourceServlet servlet;
 
-    @Before
-    public void before() throws Exception {
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
         // create the servlet to test
         servlet = new ElementsDataSourceServlet();
         Utils.setInternalState(servlet, "expressionResolver", expressionResolver);
     }
 
     @Test
-    public void testNoConfig()
+    void testNoConfig()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_NO_CONFIG);
         assertDataSource(dataSource, new String[0], new String[0]);
     }
 
     @Test
-    public void testFragmentPathNonExisting()
+    void testFragmentPathNonExisting()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_FRAGMENT_PATH_NON_EXISTING);
         assertDataSource(dataSource, new String[0], new String[0]);
     }
 
     @Test
-    public void testComponentPathNonExisting()
+    void testComponentPathNonExisting()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_COMPONENT_PATH__NON_EXISTING);
         assertDataSource(dataSource, new String[0], new String[0]);
     }
 
     @Test
-    public void testFragmentPathInvalid()
+    void testFragmentPathInvalid()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_FRAGMENT_PATH_INVALID);
         assertDataSource(dataSource, new String[0], new String[0]);
     }
 
     @Test
-    public void testComponentPathInvalid()
+    void testComponentPathInvalid()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_COMPONENT_PATH_INVALID);
         assertDataSource(dataSource, new String[0], new String[0]);
     }
 
     @Test
-    public void testComponentPathInvalidFragmentPath()
+    void testComponentPathInvalidFragmentPath()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_COMPONENT_PATH_INVALID_FRAGMENT_PATH);
         assertDataSource(dataSource, new String[0], new String[0]);
     }
 
     @Test
-    public void testFragmentPathTextOnly()
+    void testFragmentPathTextOnly()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_FRAGMENT_PATH_TEXT_ONLY);
         assertDataSource(dataSource, ELEMENT_NAMES, ELEMENT_TITLES);
     }
 
     @Test
-    public void testComponentPathTextOnly()
+    void testComponentPathTextOnly()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_COMPONENT_PATH_TEXT_ONLY);
         assertDataSource(dataSource, ELEMENT_NAMES, ELEMENT_TITLES);
     }
 
     @Test
-    public void testFragmentPathStructured()
+    void testFragmentPathStructured()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_FRAGMENT_PATH_STRUCTURED);
         assertDataSource(dataSource, ELEMENT_NAMES, ELEMENT_TITLES);
     }
 
     @Test
-    public void testComponentPathStructured()
+    void testComponentPathStructured()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_COMPONENT_PATH_STRUCTURED);
         assertDataSource(dataSource, ELEMENT_NAMES, ELEMENT_TITLES);
     }
 
     @Test
-    public void testComponentPathStructuredDisplayModeSingle()
+    void testComponentPathStructuredDisplayModeSingle()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_COMPONENT_PATH_STRUCTURED_DISPLAY_MODE_SINGLE);
         assertDataSource(dataSource, ELEMENT_NAMES_MULTILINE_TEXT_ONLY, ELEMENT_TITLES_MULTILINE_TEXT_ONLY);
     }
 
     @Test
-    public void testFragmentPathOverride()
+    void testFragmentPathOverride()
             throws ServletException, IOException {
         DataSource dataSource = getDataSource(servlet, DS_FRAGMENT_PATH_OVERRIDE);
         assertDataSource(dataSource, ELEMENT_NAMES, ELEMENT_TITLES);
