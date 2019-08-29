1f62f30e273081d4dc18ba3fc648bb4de97d58aa
Pankaj Patil <pankaj.patil2099@hotmail.com>
2019-08-29 18:16:18 +0530

    Fixed TestCases

--- bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/models/v2/PageImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/models/v2/PageImplTest.java
@@ -18,6 +18,7 @@ package com.adobe.cq.wcm.core.components.internal.models.v2;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
@@ -64,7 +65,7 @@ public class PageImplTest extends com.adobe.cq.wcm.core.components.internal.mode
     public void testPage() throws ParseException {
         Page page = getPageUnderTest(PAGE);
         Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
         calendar.setTime(sdf.parse("2016-01-20T10:33:36.000+0100"));
         assertEquals(page.getLastModifiedDate().getTime(), calendar.getTime());
         assertEquals("en-GB", page.getLanguage());
