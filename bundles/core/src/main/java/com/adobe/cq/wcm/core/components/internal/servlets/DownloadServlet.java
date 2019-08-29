18b61c9f4e4499e49ca9872deff0d2b11723b0c0
Joerg Hoh <joerg@joerghoh.de>
2019-08-29 16:39:18 +0200

    improve testcases, and fix remarks by Burkhardt

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/servlets/DownloadServlet.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/servlets/DownloadServlet.java
@@ -71,7 +71,7 @@ public class DownloadServlet extends SlingAllMethodsServlet {
     public static final String INLINE_SELECTOR = "inline";
 
     private static final String CONTENT_DISPOSITION_HEADER = "Content-Disposition";
-    private static final String RFC_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
+    protected static final String RFC_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
 
     @Override
     protected void doGet(@Nonnull SlingHttpServletRequest request, @Nonnull SlingHttpServletResponse response)
@@ -87,20 +87,21 @@ public class DownloadServlet extends SlingAllMethodsServlet {
             }
         } else {
             
-            if (checkLastModifiedAfter(asset.getLastModified(), request)) {
+            if (isUnchanged(asset.getLastModified(), request)) {
                 response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
                 return;
             }
-            Optional<InputStream> inputStream = null;
+            Optional<InputStream> inputStream = Optional.empty();
             try {
                 inputStream = getInputStream(asset);
                 if (!inputStream.isPresent()) {
                     response.sendError(HttpServletResponse.SC_NOT_FOUND);
                     return;
                 }
+                response.setStatus(HttpServletResponse.SC_OK);
                 sendResponse(inputStream.get(), asset.getOriginal().getSize(), asset.getMimeType(), asset.getName(), asset.getLastModified(), response, checkForInlineSelector(request));
             } finally {
-                if (inputStream != null && inputStream.isPresent()) {
+                if (inputStream.isPresent()) {
                     inputStream.get().close();
                 }
             }
@@ -116,12 +117,13 @@ public class DownloadServlet extends SlingAllMethodsServlet {
 
             Calendar calendar = valueMap.get(JcrConstants.JCR_LASTMODIFIED, Calendar.class);
             if (calendar != null) {
-                if (checkLastModifiedAfter(calendar.getTimeInMillis(), request)) {
+                if (isUnchanged(calendar.getTimeInMillis(), request)) {
+                    LOG.info("sending SC_NOT_MODIFIED for {}", request.getResource().getPath());
                     response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
                     return;
                 }
             }
-            Optional<InputStream> inputStream = null;
+            Optional<InputStream> inputStream = Optional.empty();
             try {
                 inputStream = getInputStream(downloadDataResource);
                 if (!inputStream.isPresent()) {
@@ -129,14 +131,19 @@ public class DownloadServlet extends SlingAllMethodsServlet {
                     return;
                 }
                 long size = getResourceSize(downloadDataResource);
+                long timestamp = 0;
+                if (calendar != null) {
+                    timestamp = calendar.getTimeInMillis();
+                }
                 String mimeType = valueMap.get(JcrConstants.JCR_MIMETYPE, String.class);
                 if (StringUtils.isNotEmpty(mimeType)) {
-                    sendResponse(inputStream.get(), size, mimeType, filename, calendar.getTimeInMillis(), response, checkForInlineSelector(request));
+                    response.sendError(HttpServletResponse.SC_OK);
+                    sendResponse(inputStream.get(), size, mimeType, filename, timestamp, response, checkForInlineSelector(request));
                 } else {
                     response.sendError(HttpServletResponse.SC_NOT_FOUND);
                 }
             } finally {
-                if (inputStream != null && inputStream.isPresent()) {
+                if (inputStream.isPresent()) {
                     inputStream.get().close();
                 } 
             }
@@ -148,8 +155,8 @@ public class DownloadServlet extends SlingAllMethodsServlet {
     /**
      * Determine the size of a binary resource just by using the JCR API; this hopefully avoids to read
      * the complete InputStream to determine the actual size of the binary.
-     * @param resource
-     * @return
+     * @param resource the actual resource
+     * @return the size in byte, -1 if an error occurs, the resource is not backed by a JCR node, or if there is not JCR_DATA property
      */
     private long getResourceSize (Resource resource) {
         
@@ -200,10 +207,12 @@ public class DownloadServlet extends SlingAllMethodsServlet {
         } else {
             response.setHeader(CONTENT_DISPOSITION_HEADER, "attachment; filename=\"" + filename + "\"");
         }
-        response.setHeader(HttpConstants.HEADER_LAST_MODIFIED, getLastModifiedDate(lastModifiedDate));
-        ServletOutputStream outputStream = response.getOutputStream();
-        IOUtils.copy(stream, outputStream);
-        outputStream.close();
+        if (lastModifiedDate > 0) {
+            response.setHeader(HttpConstants.HEADER_LAST_MODIFIED, getLastModifiedDate(lastModifiedDate));
+        }
+        try (ServletOutputStream outputStream = response.getOutputStream()) {
+            IOUtils.copy(stream, outputStream);
+        }
     }
 
     private String getLastModifiedDate(long lastModifiedDate) {
@@ -212,7 +221,7 @@ public class DownloadServlet extends SlingAllMethodsServlet {
         return df.format(lastModifiedDate);
     }
 
-    private boolean checkLastModifiedAfter(long lastModifiedDate, SlingHttpServletRequest request) {
+    private boolean isUnchanged(long lastModifiedDate, SlingHttpServletRequest request) {
         boolean match = false;
         if (Collections.list(request.getHeaderNames()).contains(HttpConstants.HEADER_IF_MODIFIED_SINCE)) {
             String headerDate = request.getHeader(HttpConstants.HEADER_IF_MODIFIED_SINCE);
