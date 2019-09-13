1987b88935aa01f717840aece66fa398724fd05f
Richard Hand <pid90611@adobe.com>
2019-09-13 11:10:37 +0200

    [Embed] Finalize Embed Component
    - bumps package version 12.9.0 -> 12.10.0

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/Embed.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/Embed.java
@@ -25,7 +25,7 @@ import com.adobe.cq.wcm.core.components.services.embed.UrlProcessor;
 /**
  * Defines the {@code Embed} Sling Model used for the {@code /apps/core/wcm/components/embed} component.
  *
- * @since com.adobe.cq.wcm.core.components.models 12.9.0
+ * @since com.adobe.cq.wcm.core.components.models 12.10.0
  */
 @ConsumerType
 public interface Embed extends ComponentExporter {
@@ -33,27 +33,27 @@ public interface Embed extends ComponentExporter {
     /**
      * Enumeration of the supported embed input types.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     enum Type {
         /**
          * Embed type for input from a URL.
          *
-         * @since com.adobe.cq.wcm.core.components.models 12.9.0
+         * @since com.adobe.cq.wcm.core.components.models 12.10.0
          */
         URL("url"),
 
         /**
          * Embed type for input from an embeddable.
          *
-         * @since com.adobe.cq.wcm.core.components.models 12.9.0
+         * @since com.adobe.cq.wcm.core.components.models 12.10.0
          */
         EMBEDDABLE("embeddable"),
 
         /**
          * Embed type for input from a HTML snippet.
          *
-         * @since com.adobe.cq.wcm.core.components.models 12.9.0
+         * @since com.adobe.cq.wcm.core.components.models 12.10.0
          */
         HTML("html");
 
@@ -69,7 +69,7 @@ public interface Embed extends ComponentExporter {
          *
          * @param value the string representation for which an enum value should be returned
          * @return the corresponding enum value, if one was found, {@code null} otherwise
-         * @since com.adobe.cq.wcm.core.components.models 12.9.0
+         * @since com.adobe.cq.wcm.core.components.models 12.10.0
          */
         public static Type fromString(String value) {
             for (Type type : values()) {
@@ -84,21 +84,21 @@ public interface Embed extends ComponentExporter {
     /**
      * Name of the configuration policy property that indicates whether the url input type is disabled.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_DESIGN_URL_DISABLED = "urlDisabled";
 
     /**
      * Name of the configuration policy property that indicates whether the embeddable input type is disabled.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_DESIGN_EMBEDDABLES_DISABLED = "embeddablesDisabled";
 
     /**
      * Name of the configuration policy property that indicates whether the html input type is disabled.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_DESIGN_HTML_DISABLED = "htmlDisabled";
 
@@ -106,42 +106,42 @@ public interface Embed extends ComponentExporter {
      * Name of the configuration policy property that defines the embeddables that are allowed to be selected
      * by an author.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_DESIGN_ALLOWED_EMBEDDABLES = "allowedEmbeddables";
 
     /**
      * Name of the resource property that defines the embed input {@link Type}.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_TYPE = "type";
 
     /**
      * Name of the resource property that defines the URL of an embeddable item.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_URL = "url";
 
     /**
      * Name of the resource property that defines an embeddable HTML snippet.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_HTML = "html";
 
     /**
      * Name of the resource property that defines the resource type of an embeddable.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String PN_EMBEDDABLE_RESOURCE_TYPE = "embeddableResourceType";
 
     /**
      * Resource type of an embeddable.
      *
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     String RT_EMBEDDABLE_V1 = "core/wcm/components/embed/v1/embed/embeddable";
 
@@ -149,7 +149,7 @@ public interface Embed extends ComponentExporter {
      * Returns the embed input {@link Type}.
      *
      * @return the embed input {@link Type}, or {@code null} if no type is found
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     @Nullable
     default Type getType() {
@@ -161,7 +161,7 @@ public interface Embed extends ComponentExporter {
      *
      * @return The URL of the embeddable item, if type is {@link Type#URL}
      *         and the url type is not disabled via policy configuration, {@code null} otherwise
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     @Nullable
     default String getUrl() {
@@ -172,7 +172,7 @@ public interface Embed extends ComponentExporter {
      * Returns the result from the processor that can process the given URL.
      *
      * @return The result from the processor that can process the given URL, {@code null} if processing is not possible
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     @Nullable
     default UrlProcessor.Result getResult() {
@@ -184,7 +184,7 @@ public interface Embed extends ComponentExporter {
      *
      * @return The resource type of the embeddable, if type is {@link Type#EMBEDDABLE}
      *         and the embeddable type is not disabled via policy configuration, {@code null} otherwise
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     @Nullable
     default String getEmbeddableResourceType() {
@@ -196,7 +196,7 @@ public interface Embed extends ComponentExporter {
      *
      * @return An embeddable HTML {@link String}, if type is {@link Type#HTML}
      *         and the html type is not disabled via policy configuration, {@code null} otherwise
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     @Nullable
     default String getHtml() {
@@ -205,7 +205,7 @@ public interface Embed extends ComponentExporter {
 
     /**
      * @see ComponentExporter#getExportedType()
-     * @since com.adobe.cq.wcm.core.components.models 12.9.0
+     * @since com.adobe.cq.wcm.core.components.models 12.10.0
      */
     @NotNull
     @Override
