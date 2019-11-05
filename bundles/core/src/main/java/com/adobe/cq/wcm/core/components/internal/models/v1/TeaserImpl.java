19e4cff87cdf44e1990fc1fb1a0565593ad39045
jckautzmann <jkautzma@adobe.com>
2019-11-05 16:11:27 +0100

    GH-431 - [Teaser] UOE is thrown during json serialization (#829)
    - exclude unused action properties from the JSON rendering

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/TeaserImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/TeaserImpl.java
@@ -51,6 +51,7 @@ import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.components.Component;
 import com.day.cq.wcm.api.designer.Style;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {Teaser.class, ComponentExporter.class}, resourceType = TeaserImpl.RESOURCE_TYPE)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME , extensions = ExporterConstants.SLING_MODEL_EXTENSION)
@@ -193,42 +194,8 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
     private void populateActions() {
         Resource actionsNode = resource.getChild(Teaser.NN_ACTIONS);
         if (actionsNode != null) {
-            for (Resource action : actionsNode.getChildren()) {
-                actions.add(new ListItem() {
-
-                    private ValueMap properties = action.getValueMap();
-                    private String title = properties.get(PN_ACTION_TEXT, String.class);
-                    private String url = properties.get(PN_ACTION_LINK, String.class);
-                    private Page page = null;
-                    {
-                        if (url != null && url.startsWith("/")) {
-                            page = pageManager.getPage(url);
-                        }
-                    }
-
-                    @Nullable
-                    @Override
-                    public String getTitle() {
-                        return title;
-                    }
-
-                    @Nullable
-                    @Override
-                    @JsonIgnore
-                    public String getPath() {
-                        return url;
-                    }
-
-                    @Nullable
-                    @Override
-                    public String getURL() {
-                        if (page != null) {
-                            return Utils.getURL(request, page);
-                        } else {
-                            return url;
-                        }
-                    }
-                });
+            for (Resource actionRes : actionsNode.getChildren()) {
+                actions.add(new Action(actionRes));
             }
         }
     }
@@ -302,5 +269,45 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
     @Override
     public String getExportedType() {
         return request.getResource().getResourceType();
+    }
+
+    @JsonIgnoreProperties({"path", "description", "lastModified", "name"})
+    public class Action implements ListItem {
+        ValueMap properties;
+        String title;
+        String url;
+        Page page;
+
+        public Action(Resource actionRes) {
+            properties = actionRes.getValueMap();
+            title = properties.get(PN_ACTION_TEXT, String.class);
+            url = properties.get(PN_ACTION_LINK, String.class);
+            if (url != null && url.startsWith("/")) {
+                page = pageManager.getPage(url);
+            }
+        }
+
+        @Nullable
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Nullable
+        @Override
+        public String getPath() {
+            return url;
+        }
+
+        @Nullable
+        @Override
+        public String getURL() {
+            if (page != null) {
+                return Utils.getURL(request, page);
+            } else {
+                return url;
+            }
+        }
+
     }
 }
