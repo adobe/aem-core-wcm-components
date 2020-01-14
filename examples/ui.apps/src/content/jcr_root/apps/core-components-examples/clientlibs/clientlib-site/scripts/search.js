0f3f6cd51662eda32ca2bd791c1d1dd1ef861343
Richard Hand <pid90611@adobe.com>
2020-01-14 17:43:43 +0100

    [Library] UX changes for Core Components library
    - adds component categories to home page
    - adds handling in the search script to account for categories
    - adjusts styles

--- examples/ui.apps/src/content/jcr_root/apps/core-components-examples/clientlibs/clientlib-site/scripts/search.js
+++ examples/ui.apps/src/content/jcr_root/apps/core-components-examples/clientlibs/clientlib-site/scripts/search.js
@@ -18,17 +18,20 @@
 
     var selectors = {
         self: '[name="cmp-examples-component-search"]',
+        componentGroup: '.cmp-examples-container--component-group',
         componentTeaser: '.cmp-examples-teaser--component',
         noResults: '.cmp-examples-text--no-results'
     };
 
     var search;
+    var componentGroups;
     var componentTeasers;
     var noResults;
 
     function updateResults(token) {
         var total = componentTeasers.length;
         var count = 0;
+
         componentTeasers.forEach(function(teaser) {
             var teaserText = teaser.textContent || teaser.innerText;
             if (teaserText.toUpperCase().indexOf(token.toUpperCase()) > -1) {
@@ -36,6 +39,25 @@
             } else {
                 teaser.style.display = "none";
                 count++;
+            }
+        });
+
+        componentGroups.forEach(function(group) {
+            var groupVisible = false;
+            var groupTeasers = group.querySelectorAll(selectors.componentTeaser);
+            groupTeasers = [].slice.call(groupTeasers);
+
+            for (var i = 0; i < groupTeasers.length; i++) {
+                if (groupTeasers[i].style.display === "") {
+                    groupVisible = true;
+                    break;
+                }
+            }
+
+            if (groupVisible) {
+                group.style.display = "";
+            } else {
+                group.style.display = "none";
             }
         });
 
@@ -47,6 +69,8 @@
     document.addEventListener('DOMContentLoaded', function() {
         search = document.querySelector(selectors.self);
         if (search) {
+            componentGroups = document.querySelectorAll(selectors.componentGroup);
+            componentGroups = [].slice.call(componentGroups);
             componentTeasers = document.querySelectorAll(selectors.componentTeaser);
             componentTeasers = [].slice.call(componentTeasers);
             noResults = document.querySelector(selectors.noResults);
