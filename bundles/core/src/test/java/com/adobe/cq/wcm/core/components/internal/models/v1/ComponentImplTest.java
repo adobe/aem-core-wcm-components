5a8ee821aee20f2b364cb64053dd21124765f2cb
Richard Hand <pid90611@adobe.com>
2020-02-11 19:45:20 +0100

    Configurable IDs for all components
    - adds test coverage for IDs of referenced components

--- bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/models/v1/ComponentImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/models/v1/ComponentImplTest.java
@@ -16,35 +16,50 @@
 package com.adobe.cq.wcm.core.components.internal.models.v1;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.scripting.SlingBindings;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import com.adobe.cq.sightly.WCMBindings;
 import com.adobe.cq.wcm.core.components.context.CoreComponentTestContext;
 import com.adobe.cq.wcm.core.components.models.Component;
+import com.day.cq.wcm.api.components.ComponentContext;
+import com.day.cq.wcm.api.Page;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(AemContextExtension.class)
 public class ComponentImplTest {
 
-    private static final String TEST_BASE = "/title";
-    private static final String TEST_PAGE = "/content/title";
-    private static final String TITLE_RESOURCE_JCR_TITLE = TEST_PAGE + "/jcr:content/par/title-jcr-title";
+    private static final String TEST_BASE = "/experiencefragment";
+    private static final String TEST_PAGE = "/content/mysite/page";
+    private static final String TEST_PAGE_EN = "/content/mysite/en/page";
+    private static final String XF_COMPONENT_1 = TEST_PAGE + "/jcr:content/root/xf-component-1";
+    private static final String XF_COMPONENT_10 = TEST_PAGE_EN + "/jcr:content/root/xf-component-10";
 
     private final AemContext context = CoreComponentTestContext.newAemContext();
 
     @BeforeEach
     void setUp() {
-        context.load().json(TEST_BASE + "/test-content.json", TEST_PAGE);
+        context.load().json(TEST_BASE + CoreComponentTestContext.TEST_CONTENT_JSON, "/content");
     }
 
     @Test
     public void testComponentId() {
-        Component component = getComponentUnderTest(TITLE_RESOURCE_JCR_TITLE);
-        Assert.assertEquals("ID mismatch", "title-af55b2ad84", component.getId());
+        Component component = getComponentUnderTest(XF_COMPONENT_1);
+        Assert.assertEquals("ID mismatch", "experiencefragment-77cae01492", component.getId());
+    }
+
+    @Test
+    public void testReferencedComponentId() {
+        Component component = getReferencedComponentUnderTest(XF_COMPONENT_1, TEST_PAGE_EN, XF_COMPONENT_10);
+        Assert.assertEquals("ID mismatch", "experiencefragment-5fba9a0f26", component.getId());
     }
 
     private Component getComponentUnderTest(String resourcePath, Object ... properties) {
@@ -53,6 +68,25 @@ public class ComponentImplTest {
             context.contentPolicyMapping(resource.getResourceType(), properties);
         }
         MockSlingHttpServletRequest request = context.request();
+        return request.adaptTo(Component.class);
+    }
+
+    private Component getReferencedComponentUnderTest(String resourcePath, String currentPagePath, String referencerPath, Object ... properties) {
+        Resource resource = context.currentResource(resourcePath);
+        if (resource != null && properties != null) {
+            context.contentPolicyMapping(resource.getResourceType(), properties);
+        }
+        Resource referencer = context.resourceResolver().getResource(referencerPath);
+        SlingBindings slingBindings = new SlingBindings();
+        ComponentContext componentContext = mock(ComponentContext.class);
+        ComponentContext parentContext = mock(ComponentContext.class);
+        when(parentContext.getResource()).thenReturn(referencer);
+        when(componentContext.getParent()).thenReturn(parentContext);
+        Page currentPage = context.pageManager().getPage(currentPagePath);
+        slingBindings.put(WCMBindings.COMPONENT_CONTEXT, componentContext);
+        slingBindings.put(WCMBindings.CURRENT_PAGE, currentPage);
+        MockSlingHttpServletRequest request = context.request();
+        request.setAttribute(SlingBindings.class.getName(), slingBindings);
         return request.adaptTo(Component.class);
     }
 }
