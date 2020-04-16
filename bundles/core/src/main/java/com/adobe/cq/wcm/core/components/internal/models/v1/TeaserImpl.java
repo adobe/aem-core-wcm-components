b00c1efbc357fa1215878fd574f63d713da09477
dimitrios.soltysiak <dimitrios.soltysiak@cognifide.com>
2020-04-16 23:37:09 +0200

    else reverted

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/TeaserImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/TeaserImpl.java
@@ -144,6 +144,8 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
                 } else if (actionsEnabled && !actions.isEmpty()) {
                     title = actions.get(0).getTitle();
                     linkURL = actions.get(0).getURL();
+                } else {
+                    title = null;
                 }
             }
         }
