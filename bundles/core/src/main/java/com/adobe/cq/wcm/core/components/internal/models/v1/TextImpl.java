8bee2877af337589587ca32090fb17933b122899
Vlad Băilescu <bailescu@adobe.com>
2020-04-27 13:04:27 +0300

    Improved null handling

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/TextImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/TextImpl.java
@@ -17,6 +17,7 @@ package com.adobe.cq.wcm.core.components.internal.models.v1;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
@@ -70,7 +71,6 @@ public class TextImpl extends AbstractComponentImpl implements Text {
 
     @Override
     public String getDataLayerText() {
-        String text = getText();
-        return text != null ? text : "";
+        return StringUtils.defaultIfEmpty(this.text, StringUtils.EMPTY);
     }
 }
