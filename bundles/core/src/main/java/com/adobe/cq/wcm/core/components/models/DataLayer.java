66b64277bfeb6963a85fbf30fee5ddd5ac01831d
Burkhard Pauli <pauli@adobe.com>
2020-05-02 12:57:03 +0200

    Remove data attributes from components (#998)
    * Remove data attributes from components
    
    - remove data attributes from HTL output in case the data layer is disabled
    - make getString return empty string in case data layer is disabled
    - fix broken link in title example content
    - adapt unit test to test getString output
    
    * Remove data attributes from components
    
    - return null from getDataLayer in case data layer is disabled
    - adapt unit tests
    
    * Remove data attributes from components
    
    - add unit test for DataLayerSerializer

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/DataLayer.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/DataLayer.java
@@ -15,16 +15,12 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.wcm.core.components.models;
 
-import java.util.Map;
-
 import org.apache.sling.api.resource.Resource;
+import org.jetbrains.annotations.Nullable;
 import org.osgi.annotation.versioning.ConsumerType;
 
 import com.adobe.cq.wcm.core.components.internal.jackson.DataLayerSerializer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonRootName;
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 /**
@@ -167,6 +163,7 @@ public interface DataLayer {
      * @return JSON string
      */
     @JsonIgnore
+    @Nullable
     default String getString() {
         throw new UnsupportedOperationException();
     }
