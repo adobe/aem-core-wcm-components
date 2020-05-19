903446ff438d4c67085257bdb1ccc9d3106de4f6
Burkhard Pauli <pauli@adobe.com>
2020-05-19 17:05:48 +0200

    [DataLayer] Automatically update the data layer clientlib
    - add npm dependency for adobe-client-data-layer
    - use aem-clientlib-generator to automatically create aem clientlib for the datalayer

---
+++ content/clientlib.config.js
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright 2020 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+module.exports = {
+    context: __dirname,
+    clientLibRoot: "src/content/jcr_root/apps/core/wcm/components/commons/datalayer/clientlibs",
+    libs: [
+        {
+            name: "core.wcm.components.commons.datalayer.v1",
+            serializationFormat: "xml",
+            allowProxy: true,
+            longCacheKey: "${project.version}",
+            assets: {
+                js: [
+                    "src/scripts/datalayer/polyfill.js",
+                    "node_modules/@adobe/adobe-client-data-layer/dist/adobe-client-data-layer.min.js",
+                    "src/scripts/datalayer/datalayer.js"
+                ]
+            }
+        }
+    ]
+};
