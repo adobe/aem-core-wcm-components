bdc02d9ed724038f2b2f104c3778ef5e9889d5ed
Kyle Giovannetti <kyle.giovannetti@dal.ca>
2020-06-17 00:40:44 -0300

    Update `AbstractComponentImpl` to use `ComponentUtils`
    Removes the ID generating and data layer enabled check implementations
    from the `AbstractComponentImpl`, and references the utility methods
    available in `ComponentUtils` instead.

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/AbstractComponentImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/AbstractComponentImpl.java
@@ -15,37 +15,25 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.wcm.core.components.internal.models.v1;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.caconfig.ConfigurationBuilder;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.adobe.cq.wcm.core.components.internal.DataLayerConfig;
-import com.adobe.cq.wcm.core.components.internal.Utils;
 import com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.ComponentDataImpl;
 import com.adobe.cq.wcm.core.components.models.Component;
 import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
+import com.adobe.cq.wcm.core.components.util.ComponentUtils;
 import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
-import com.day.cq.wcm.api.Template;
 import com.day.cq.wcm.api.components.ComponentContext;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import static com.adobe.cq.wcm.core.components.internal.Utils.ID_SEPARATOR;
 
 /**
  * Abstract class that can be used as a base class for {@link Component} implementations.
  */
 public abstract class AbstractComponentImpl implements Component {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractComponentImpl.class);
 
     @SlingObject
     protected Resource resource;
@@ -70,15 +58,7 @@ public abstract class AbstractComponentImpl implements Component {
     @Override
     public String getId() {
         if (id == null) {
-            if (resource != null) {
-                ValueMap properties = resource.getValueMap();
-                id = properties.get(Component.PN_ID, String.class);
-            }
-            if (StringUtils.isEmpty(id)) {
-                id = generateId();
-            } else {
-                id = StringUtils.replace(StringUtils.normalizeSpace(StringUtils.trim(id)), " ", ID_SEPARATOR);
-            }
+            this.id = ComponentUtils.getPropertyOrGeneratedId(this.resource, this.currentPage, this.componentContext);
         }
         return id;
     }
@@ -90,76 +70,6 @@ public abstract class AbstractComponentImpl implements Component {
     }
 
     /**
-     * Returns an auto generated component ID.
-     *
-     * The ID is the first 10 characters of an SHA-1 hexadecimal hash of the component path,
-     * prefixed with the component name. Example: title-810f3af321
-     *
-     * If the component is referenced, the path is taken to be a concatenation of the component path,
-     * with the path of the first parent context resource that exists on the page or in the template.
-     * This ensures the ID is unique if the same component is referenced multiple times on the same page or template.
-     *
-     * Collision
-     * ---------
-     * c = expected collisions
-     * c ~= (i^2)/(2m) - where i is the number of items and m is the number of possibilities for each item.
-     * m = 16^n - for a hexadecimal string, where n is the number of characters.
-     *
-     * For i = 1000 components with the same name, and n = 10 characters:
-     *
-     * c ~= (1000^2)/(2*(16^10))
-     * c ~= 0.00000045
-     *
-     * @return the auto generated component ID
-     */
-    private String generateId() {
-        String resourceType = resource.getResourceType();
-        String prefix = StringUtils.substringAfterLast(resourceType, "/");
-        String path = resource.getPath();
-        if (currentPage != null && componentContext != null) {
-            PageManager pageManager = currentPage.getPageManager();
-            Page containingPage = pageManager.getContainingPage(resource);
-            Template template = currentPage.getTemplate();
-            boolean inCurrentPage = (containingPage != null && StringUtils.equals(containingPage.getPath(), currentPage.getPath()));
-            boolean inTemplate = (template != null && path.startsWith(template.getPath()));
-            if (!inCurrentPage && !inTemplate) {
-                ComponentContext parentContext = componentContext.getParent();
-                while (parentContext != null) {
-                    Resource parentContextResource = parentContext.getResource();
-                    if (parentContextResource != null) {
-                        Page parentContextPage = pageManager.getContainingPage(parentContextResource);
-                        inCurrentPage = (parentContextPage != null && StringUtils.equals(parentContextPage.getPath(), currentPage.getPath()));
-                        inTemplate = (template != null && parentContextResource.getPath().startsWith(template.getPath()));
-                        if (inCurrentPage || inTemplate) {
-                            path = parentContextResource.getPath().concat(resource.getPath());
-                            break;
-                        }
-                    }
-                    parentContext = parentContext.getParent();
-                }
-            }
-
-        }
-
-        return Utils.generateId(prefix, path);
-    }
-
-    private boolean isDataLayerEnabled() {
-        if (dataLayerEnabled == null) {
-            dataLayerEnabled = false;
-            if (resource != null) {
-                ConfigurationBuilder builder = resource.adaptTo(ConfigurationBuilder.class);
-                if (builder != null) {
-                    DataLayerConfig dataLayerConfig = builder.as(DataLayerConfig.class);
-                    dataLayerEnabled = dataLayerConfig.enabled();
-                }
-            }
-        }
-        return dataLayerEnabled;
-    }
-
-
-    /**
      * See {@link Component#getData()}
      *
      * @return The component data
@@ -167,16 +77,18 @@ public abstract class AbstractComponentImpl implements Component {
     @Override
     @Nullable
     public ComponentData getData() {
-        if (!isDataLayerEnabled()) {
-            return null;
-        }
         if (componentData == null) {
-            componentData = getComponentData();
+            if (this.dataLayerEnabled == null) {
+                this.dataLayerEnabled = ComponentUtils.isDataLayerEnabled(this.resource);
+            }
+            if (this.dataLayerEnabled) {
+                componentData = getComponentData();
+            }
         }
         return componentData;
     }
 
-    /**
+    /*
      * Data layer specific methods. Each component can choose to implement some of these, to override or feed the data model.
      */
 
