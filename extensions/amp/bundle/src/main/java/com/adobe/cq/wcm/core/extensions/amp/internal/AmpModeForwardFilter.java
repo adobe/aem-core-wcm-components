8b08449f92d25611f81b5bf18ece963b702bc0cf
Burkhard Pauli <pauli@adobe.com>
2020-08-07 17:08:45 +0200

    AmpModeForwardFilter breaks Adobe Campaign integration (fixes #1126)
    - add check for dedicated amp selector
    - add unit test to test campaign selector

--- extensions/amp/bundle/src/main/java/com/adobe/cq/wcm/core/extensions/amp/internal/AmpModeForwardFilter.java
+++ extensions/amp/bundle/src/main/java/com/adobe/cq/wcm/core/extensions/amp/internal/AmpModeForwardFilter.java
@@ -20,6 +20,8 @@ import static com.adobe.cq.wcm.core.extensions.amp.internal.AmpUtil.AMP_SELECTOR
 import static com.adobe.cq.wcm.core.extensions.amp.internal.AmpUtil.DOT;
 import static com.adobe.cq.wcm.core.extensions.amp.internal.AmpUtil.NO_AMP;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.request.RequestDispatcherOptions;
 import org.apache.sling.api.servlets.HttpConstants;
@@ -46,7 +48,6 @@ import java.io.IOException;
         configurationPid = "com.adobe.cq.wcm.core.components.internal.services.amp.AmpModeForwardFilter",
         property = {
                 ServletResolverConstants.SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_GET,
-                ServletResolverConstants.SLING_SERVLET_EXTENSIONS + "=amp.html",
                 EngineConstants.SLING_FILTER_SCOPE + "=" + EngineConstants.FILTER_SCOPE_REQUEST,
                 EngineConstants.SLING_FILTER_PATTERN + "=/content/.*",
                 "service.ranking:Integer=1000"
@@ -65,57 +66,55 @@ public class AmpModeForwardFilter implements Filter {
 
         SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
 
-        // Read the full selector string from the request.
-        String selectors = slingRequest.getRequestPathInfo().getSelectorString();
-        if (selectors == null) {
-            selectors = "";
-        }
-
-        boolean hasAmpSelector = selectors.contains(AMP_SELECTOR);
+        String[] selectorArray = slingRequest.getRequestPathInfo().getSelectors();
+        boolean hasAmpSelector = ArrayUtils.contains(selectorArray, AMP_SELECTOR);
 
         String ampMode = AmpUtil.getAmpMode(slingRequest);
+        if (hasAmpSelector || ampMode.equals(AMP_ONLY)) {
 
-        if (hasAmpSelector && (ampMode.equals(NO_AMP) || ampMode.isEmpty())) {
+            String selectors = StringUtils.defaultIfEmpty(slingRequest.getRequestPathInfo().getSelectorString(), StringUtils.EMPTY);
+            if (hasAmpSelector && (ampMode.equals(NO_AMP) || ampMode.isEmpty())) {
 
-            // Remove the amp selector.
-            String newSelectors;
-            if (selectors.contains(DOT + AMP_SELECTOR)) {
-                newSelectors = selectors.replace(DOT + AMP_SELECTOR, "");
-            } else if (selectors.contains(AMP_SELECTOR + DOT)) {
-                newSelectors = selectors.replace(AMP_SELECTOR + DOT, "");
-            } else {
-                newSelectors = selectors.replace(AMP_SELECTOR, "");
-            }
+                // Remove the amp selector.
+                String newSelectors;
+                if (selectors.contains(DOT + AMP_SELECTOR)) {
+                    newSelectors = selectors.replace(DOT + AMP_SELECTOR, "");
+                } else if (selectors.contains(AMP_SELECTOR + DOT)) {
+                    newSelectors = selectors.replace(AMP_SELECTOR + DOT, "");
+                } else {
+                    newSelectors = selectors.replace(AMP_SELECTOR, "");
+                }
 
-            // Apply the updated selectors.
-            RequestDispatcherOptions options = new RequestDispatcherOptions();
-            options.setReplaceSelectors(newSelectors);
+                // Apply the updated selectors.
+                RequestDispatcherOptions options = new RequestDispatcherOptions();
+                options.setReplaceSelectors(newSelectors);
 
-            if (forward(slingRequest, response, options)) {
-                return;
-            }
-        } else if (!hasAmpSelector && ampMode.equals(AMP_ONLY)) {
+                if (forward(slingRequest, response, options)) {
+                    return;
+                }
+            } else if (!hasAmpSelector && ampMode.equals(AMP_ONLY)) {
 
-            // Add the 'amp' selector to the dispatcher. Making sure to put it at the beginning of the selector list.
-            RequestDispatcherOptions options = new RequestDispatcherOptions();
-            if (!selectors.isEmpty()) {
-                options.setReplaceSelectors(AMP_SELECTOR + DOT + selectors);
-            } else {
-                options.setAddSelectors(AMP_SELECTOR);
-            }
+                // Add the 'amp' selector to the dispatcher. Making sure to put it at the beginning of the selector list.
+                RequestDispatcherOptions options = new RequestDispatcherOptions();
+                if (!selectors.isEmpty()) {
+                    options.setReplaceSelectors(AMP_SELECTOR + DOT + selectors);
+                } else {
+                    options.setAddSelectors(AMP_SELECTOR);
+                }
 
-            if (forward(slingRequest, response, options)) {
-                return;
-            }
-        } else if (selectors.contains(DOT + AMP_SELECTOR)) {
+                if (forward(slingRequest, response, options)) {
+                    return;
+                }
+            } else if (selectors.contains(DOT + AMP_SELECTOR)) {
 
-            // Move the amp selector to the front of the list of selectors.
-            RequestDispatcherOptions options = new RequestDispatcherOptions();
-            String newSelectors = selectors.replace(DOT + AMP_SELECTOR, "");
-            options.setReplaceSelectors(AMP_SELECTOR + DOT + newSelectors);
+                // Move the amp selector to the front of the list of selectors.
+                RequestDispatcherOptions options = new RequestDispatcherOptions();
+                String newSelectors = selectors.replace(DOT + AMP_SELECTOR, "");
+                options.setReplaceSelectors(AMP_SELECTOR + DOT + newSelectors);
 
-            if (forward(slingRequest, response, options)) {
-                return;
+                if (forward(slingRequest, response, options)) {
+                    return;
+                }
             }
         }
 
