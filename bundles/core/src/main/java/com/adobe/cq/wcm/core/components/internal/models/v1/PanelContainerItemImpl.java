8a11cea17383f991b7d23b3991ee1dbb14241058
Vlad Bailescu <vlad@bailescu.ro>
2021-01-12 18:23:20 +0200

    [Accordion][DataLayer] Accordions in Experience Fragments do not work when DataLayer is activated #1339
    * Getting the datalayer state from the current page (which gets passed as a parameter for panel container items) so that TemplatedResources render the datalayer information correctly.

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/PanelContainerItemImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/PanelContainerItemImpl.java
@@ -22,6 +22,7 @@ import org.jetbrains.annotations.NotNull;
 
 import com.adobe.cq.wcm.core.components.models.ListItem;
 import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.components.Component;
 
 /**
@@ -40,8 +41,10 @@ public class PanelContainerItemImpl extends ResourceListItemImpl implements List
      * @param resource The resource.
      * @param parentId The ID of the containing component.
      */
-    public PanelContainerItemImpl(@NotNull final Resource resource, final String parentId, Component component) {
+    public PanelContainerItemImpl(@NotNull final Resource resource, final String parentId, Component component,
+                                  Page currentPage) {
         super(resource, parentId, component);
+        setCurrentPage(currentPage);
         title = Optional.ofNullable(resource.getValueMap().get(PN_PANEL_TITLE, String.class))
             .orElseGet(() -> resource.getValueMap().get(JcrConstants.JCR_TITLE, String.class));
     }
