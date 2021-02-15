2453f36851cfdfbbd9824703e91d373b8ceb8a91
Joel Köbner <jkoebner@eggs.de>
2021-02-15 13:31:47 +0100

    feat(sites-js,fe-build): add exports for all js modules
    added main js entry points for all js sites modules and added gateway main entry point for js package that can be imported as package.json
    
    1367

---
+++ content/src/content/jcr_root/apps/core/wcm/components/main.js
@@ -0,0 +1,13 @@
+import accordionv1 from './accordion/v1/accordion/clientlibs/site/js/main';
+import imagev1 from './image/v1/image/clientlibs/site/js/main';
+import imagev2 from './image/v2/image/clientlibs/site/js/main';
+import pdfviewerv1 from './pdfviewer/v1/pdfviewer/clientlibs/site/js/main';
+import searchv1 from './search/v1/search/clientlibs/site/js/main';
+import tabsv1 from './tabs/v1/tabs/clientlibs/site/js/main';
+
+export function accordionv1() {accordionv1()};
+export function imagev1() {imagev1()};
+export function imagev2() {imagev2()};
+export function pdfviewerv1() {pdfviewerv1()};
+export function searchv1() {searchv1()};
+export function tabsv1() {tabsv1()};
