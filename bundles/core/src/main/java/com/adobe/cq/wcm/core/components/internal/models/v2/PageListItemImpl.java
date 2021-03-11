2e1c743d95a5d43ad69b5ca42ff5e4dab7201864
JC Kautzmann <jkautzma@adobe.com>
2021-03-11 15:41:19 +0100

    [Navigation][LinkHanlder][Stage3] Create Navigation v2 and integrate the new link handler feature
    - create Navigation v2 with new link feature

---
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v2/PageListItemImpl.java
@@ -0,0 +1,48 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2021 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.wcm.core.components.internal.models.v2;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.adobe.cq.wcm.core.components.commons.link.Link;
+import com.adobe.cq.wcm.core.components.internal.link.LinkHandler;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.components.Component;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class PageListItemImpl extends com.adobe.cq.wcm.core.components.internal.models.v1.PageListItemImpl {
+
+    public PageListItemImpl(@NotNull LinkHandler linkHandler,
+                            @NotNull final Page page,
+                            final String parentId,
+                            final boolean isShadowingDisabled,
+                            final Component component) {
+        super(linkHandler, page, parentId, isShadowingDisabled, component);
+    }
+
+    @Override
+    public @NotNull Link getLink() {
+        return link;
+    }
+
+    @Override
+    @JsonIgnore
+    @Deprecated
+    public String getURL() {
+        return super.getURL();
+    }
+
+}
