6bf0de31828531a5cc89070d806b51ffddd50f66
JC Kautzmann <jkautzma@adobe.com>
2021-03-22 21:28:31 +0100

    [Breadcrumb][LinkHanlder][Stage3] Create Breadcrumb v3 and integrate the new link handler feature
    - set Link<Page> as return type

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v2/PageListItemImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v2/PageListItemImpl.java
@@ -36,7 +36,7 @@ public class PageListItemImpl extends com.adobe.cq.wcm.core.components.internal.
 
     @Override
     @Nullable
-    public Link getLink() {
+    public Link<Page> getLink() {
         return link.orElse(null);
     }
 
