83c1323f6d5e53cf5dec5b92da2be9ff759ae959
friendlymahi <34920377+friendlymahi@users.noreply.github.com>
2021-05-10 08:10:24 -0500

    [StyleSystem/SPAEditor] Allow BaseComponent Sling Model to export CSS classes selected by the content author (#1421)
    * Inclusion of style system props in absence of componentContext via Sling Model
    
    Closes #1374
    
    Co-authored-by: Mahidhar Chaluvadi <mahidhar.chaluvadi@aig.com>
    Co-authored-by: Vlad <3670963+vladbailescu@users.noreply.github.com>
    Co-authored-by: Kyle Giovannetti <19987287+ky940819@users.noreply.github.com>

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/AbstractComponentImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/AbstractComponentImpl.java
@@ -18,6 +18,7 @@ package com.adobe.cq.wcm.core.components.internal.models.v1;
 import java.util.Calendar;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
@@ -32,6 +33,7 @@ import com.adobe.cq.wcm.core.components.models.Component;
 import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
 import com.adobe.cq.wcm.core.components.models.datalayer.builder.DataLayerBuilder;
 import com.adobe.cq.wcm.core.components.util.ComponentUtils;
+import com.adobe.cq.wcm.style.ComponentStyleInfo;
 import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.components.ComponentContext;
@@ -73,7 +75,7 @@ public abstract class AbstractComponentImpl implements Component {
     @ScriptVariable(injectionStrategy = InjectionStrategy.OPTIONAL)
     @Nullable
     private Page currentPage;
-
+    
     /**
      * The ID for this component.
      */
@@ -145,7 +147,22 @@ public abstract class AbstractComponentImpl implements Component {
         }
         return componentData;
     }
-
+    
+    /**
+     * See {@link Component#getAppliedCssClasses()}
+     *
+     * @return The component styles/css class names
+     */
+    @Override
+    @Nullable
+	public String getAppliedCssClasses() {
+    	
+    	return Optional.ofNullable(this.resource.adaptTo(ComponentStyleInfo.class)) 
+    			.map(ComponentStyleInfo::getAppliedCssClasses) 
+    			.filter(StringUtils::isNotBlank) 
+    			.orElse(null);		// Returning null so sling model exporters don't return anything for this property if not configured
+	}
+    
     /**
      * Override this method to provide a different data model for your component. This will be called by
      * {@link AbstractComponentImpl#getData()} in case the datalayer is activated.
