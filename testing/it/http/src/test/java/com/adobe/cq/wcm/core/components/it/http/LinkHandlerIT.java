ab3743474c37565cc6f8be149f60cb801bd31420
Burkhard Pauli <mail@bpauli.de>
2021-05-14 11:16:05 +0200

    Add IT for LinkHandler
    - add polling after restore to make sure that the resource resolver is back

--- testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/LinkHandlerIT.java
+++ testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/LinkHandlerIT.java
@@ -17,10 +17,13 @@ package com.adobe.cq.wcm.core.components.it.http;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.testing.clients.ClientException;
 import org.apache.sling.testing.clients.osgi.OsgiConsoleClient;
+import org.apache.sling.testing.clients.osgi.OsgiInstanceConfig;
+import org.apache.sling.testing.clients.util.config.impl.InstanceConfigCacheImpl;
 import org.apache.sling.testing.clients.util.poller.Polling;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
@@ -33,7 +36,6 @@ import org.junit.rules.ErrorCollector;
 import com.adobe.cq.testing.client.CQClient;
 import com.adobe.cq.testing.junit.rules.CQAuthorPublishClassRule;
 import com.adobe.cq.testing.junit.rules.CQRule;
-import com.adobe.cq.testing.junit.rules.OsgiConfigRestoreRule;
 
 public class LinkHandlerIT {
 
@@ -44,39 +46,47 @@ public class LinkHandlerIT {
     public CQRule cqBaseRule = new CQRule(cqBaseClassRule.authorRule, cqBaseClassRule.publishRule);
 
     @Rule
-    public OsgiConfigRestoreRule osgiConfigRestoreRule = new OsgiConfigRestoreRule(cqBaseClassRule.authorRule, RESOURCE_RESOLVER_FACTORY_PID);
-
-    @Rule
     public ErrorCollector collector = new ErrorCollector();
 
-    public static final String RESOURCE_RESOLVER_FACTORY_PID = "org.apache.sling.jcr.resource.internal.JcrResourceResolverFactoryImpl";
+    private static final String RESOURCE_RESOLVER_FACTORY_PID = "org.apache.sling.jcr.resource.internal.JcrResourceResolverFactoryImpl";
+    private static final int TIMEOUT = 300000;
+    private static final int DELAY = 500;
 
     static CQClient adminAuthor;
     static CQClient adminPublish;
+    static InstanceConfigCacheImpl configs;
 
     @BeforeClass
-    public static void beforeClass() {
+    public static void beforeClass() throws Exception {
         adminAuthor = cqBaseClassRule.authorRule.getAdminClient(CQClient.class);
         adminPublish = cqBaseClassRule.publishRule.getAdminClient(CQClient.class);
+        configs = new InstanceConfigCacheImpl();
     }
 
     @Test
     public void testMappedLinkedTitle() throws Exception {
+        configs.add(new OsgiInstanceConfig(adminAuthor, RESOURCE_RESOLVER_FACTORY_PID));
         configureResourceResolverFactory();
-        new Polling(() -> {
+        new Polling(getLinkCallable("/simple-page/simple-subpage.html")).poll(TIMEOUT, DELAY);
+        configs.restore();
+        new Polling(getLinkCallable("/content/core-components/simple-page/simple-subpage.html")).poll(TIMEOUT, DELAY);
+    }
+
+    private Callable<Boolean> getLinkCallable(String expectedLinkURL) {
+        return () -> {
             try {
                 String content = adminAuthor.doGet("/content/core-components/simple-page.html", 200).getContent();
-                return assertLink(content);
+                return assertLink(content, expectedLinkURL);
             } catch (ClientException e) {
                 return false;
             }
-        }).poll(50000, 500);
+        };
     }
 
-    private Boolean assertLink(String content) {
+    private Boolean assertLink(String content, String expectedLinkURL) {
         Element element = Jsoup.parse(content).select("#linked-title").select("a.cmp-title__link").first();
         if (element != null) {
-            return StringUtils.equals("/simple-page/simple-subpage.html", element.attr("href"));
+            return StringUtils.equals(expectedLinkURL, element.attr("href"));
         }
         return false;
     }
