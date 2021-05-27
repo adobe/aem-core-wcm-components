40fbcccb3765a384a11cf575004b8ed526ba40a2
msagolj <37147400+msagolj@users.noreply.github.com>
2021-05-27 16:35:26 +0200

    add check for dynamically swapped images

--- content/src/content/jcr_root/apps/core/wcm/components/image/v2/image/clientlibs/site/js/image.js
+++ content/src/content/jcr_root/apps/core/wcm/components/image/v2/image/clientlibs/site/js/image.js
@@ -231,9 +231,15 @@
                 replacement = hasWidths ? (that._properties.dmimage ? "" : ".") + getOptimalWidth(that._properties.widths) : "";
             }
             var url = that._properties.src.replace(SRC_URI_TEMPLATE_WIDTH_VAR, replacement);
-
             var imgSrcAttribute = that._elements.image.getAttribute("src");
-            if (imgSrcAttribute === null || imgSrcAttribute === EMPTY_PIXEL) {
+
+            var urlTemplateParts = that._properties.src.split(SRC_URI_TEMPLATE_WIDTH_VAR);
+            // check if image src was dynamically swapped meanwhile (e.g. by Target)
+            var isImageRefSame = imgSrcAttribute.startsWith(urlTemplateParts[0])
+                && imgSrcAttribute.endsWith(urlTemplateParts[1])
+
+            if ((isImageRefSame === true && imgSrcAttribute !== url) ||
+                (isImageRefSame === false && (imgSrcAttribute === null || imgSrcAttribute === EMPTY_PIXEL))) {
                 that._elements.image.setAttribute("src", url);
                 if (!hasWidths) {
                     window.removeEventListener("scroll", that.update);
