62756d174cb5d2295d2b14b4df8074136319607e
drudolph <drudolph@adobe.com>
2021-06-25 21:00:24 +0200

    add robots, canonical and language alternates

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v2/PageImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v2/PageImpl.java
@@ -15,10 +15,14 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.wcm.core.components.internal.models.v2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -44,6 +48,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osgi.framework.Version;
 
+import com.adobe.aem.wcm.seo.SeoTags;
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ContainerExporter;
 import com.adobe.cq.export.json.ExporterConstants;
@@ -54,6 +59,7 @@ import com.adobe.cq.wcm.core.components.internal.models.v1.RedirectItemImpl;
 import com.adobe.cq.wcm.core.components.models.HtmlPageItem;
 import com.adobe.cq.wcm.core.components.models.NavigationItem;
 import com.adobe.cq.wcm.core.components.models.Page;
+import com.adobe.cq.wcm.core.components.services.link.PathProcessor;
 import com.adobe.granite.license.ProductInfoProvider;
 import com.adobe.granite.ui.clientlibs.ClientLibrary;
 import com.adobe.granite.ui.clientlibs.HtmlLibraryManager;
@@ -116,6 +122,12 @@ public class PageImpl extends com.adobe.cq.wcm.core.components.internal.models.v
      */
     @OSGiService
     private ConfigurationResolver configurationResolver;
+
+    /**
+     * The @{@link PathProcessor} service.
+     */
+    @OSGiService
+    private PathProcessor pathProcessor;
 
     /**
      * The current component context.
@@ -310,5 +322,37 @@ public class PageImpl extends com.adobe.cq.wcm.core.components.internal.models.v
             }
         }
         return htmlPageItems;
+    }
+
+    @Override
+    public @Nullable String getCanonicalLink() {
+        try {
+            SeoTags seoTags = resource.adaptTo(SeoTags.class);
+            return seoTags != null ? seoTags.getCanonicalUrl() : null;
+        } catch (NoClassDefFoundError ex) {
+            return pathProcessor.externalize(currentPage.getPath(), request);
+        }
+    }
+
+    @Override
+    public @NotNull Map<Locale, String> getAlternateLanguageLinks() {
+        try {
+            SeoTags seoTags = resource.adaptTo(SeoTags.class);
+            return seoTags != null ? seoTags.getAlternateLanguages() : Collections.emptyMap();
+        } catch (NoClassDefFoundError ex) {
+            return Collections.emptyMap();
+        }
+    }
+
+    @Override
+    public @NotNull List<String> getRobotsTags() {
+        try {
+            SeoTags seoTags = resource.adaptTo(SeoTags.class);
+            return seoTags != null && seoTags.getRobotsTags().size() > 0
+                ? new ArrayList<>(seoTags.getRobotsTags())
+                : Collections.emptyList();
+        } catch (NoClassDefFoundError ex) {
+            return Collections.emptyList();
+        }
     }
 }
