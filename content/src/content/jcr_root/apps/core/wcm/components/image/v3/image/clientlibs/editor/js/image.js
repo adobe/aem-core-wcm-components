4c88b493c531c5d5153ac2e8667aa62bb69d4fbe
Kyle Giovannetti <kyle.giovannetti@dal.ca>
2021-10-10 22:41:41 -0300

    Fix `no-unused-vars` lint errors
    Fixes lint errors for the rule `no-unused-vars`.
    
     ----
     refs #1870

--- content/src/content/jcr_root/apps/core/wcm/components/image/v3/image/clientlibs/editor/js/image.js
+++ content/src/content/jcr_root/apps/core/wcm/components/image/v3/image/clientlibs/editor/js/image.js
@@ -13,7 +13,7 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/* global jQuery, Coral */
+/* global jQuery */
 (function($) {
     "use strict";
 
