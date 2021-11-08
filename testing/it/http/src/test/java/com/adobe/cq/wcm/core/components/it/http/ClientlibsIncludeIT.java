7db9c7939c5a05e263616411b9ac92facc759e55
JC Kautzmann <jkautzma@adobe.com>
2021-11-08 18:14:23 +0100

    GH-1880 - [Page] Load all clienlibs with async
    - make the regex of the test URL more flexible

--- testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/ClientlibsIncludeIT.java
+++ testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/ClientlibsIncludeIT.java
@@ -39,8 +39,8 @@ public class ClientlibsIncludeIT {
 
     private static CQClient adminAuthor;
     private String testPage = "/content/core-components/clientlibs-include-page";
-    private final static String REGEX_SCRIPT_ELEMENT = "<script async crossorigin=\"anonymous\" onload=\"console.log..\" src=\".*/etc.clientlibs/core/wcm/tests/components/clientlibs-include/clientlibs/site..*.min.js\"></script>";
-    private final static String REGEX_LINK_ELEMENT = "<link media=\"print\" rel=\"stylesheet\" href=\".*/etc.clientlibs/core/wcm/tests/components/clientlibs-include/clientlibs/site..*.min.css\" type=\"text/css\">";
+    private final static String REGEX_SCRIPT_ELEMENT = "<script async crossorigin=\"anonymous\" onload=\"console.log..\" src=\".*/etc.clientlibs/core/wcm/tests/components/clientlibs-include/clientlibs/site.*.js\"></script>";
+    private final static String REGEX_LINK_ELEMENT = "<link media=\"print\" rel=\"stylesheet\" href=\".*/etc.clientlibs/core/wcm/tests/components/clientlibs-include/clientlibs/site.*.css\" type=\"text/css\">";
 
     @BeforeClass
     public static void beforeClass() {
