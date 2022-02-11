adobe/aem-core-wcm-components

commit 0730456fe2414935e6d41c7502ebdc5b9c2f6554

[Teaser] Enable inheritance by default (#1953)
* Updated logic so that page inheritance works as follows :
  - if a main link is defined, the page is loaded from the main link
  - if not, if actions are enabled and defined, the page is loaded from the first action
  - if not, the page is loaded from the current page
* Updated integration tests

Co-authored-by: Vlad Bailescu <vlad@bailescu.ro>

content/src/content/jcr_root/apps/core/wcm/components/image/v3/image/clientlibs/editor/js/image.js
@@ -75,8 +75,7 @@
             $altGroup = $dialogContent.find(".cmp-image__editor-alt");
             $altTextField = $dialogContent.find(".cmp-image__editor-alt-text");
             $linkURLGroup = $dialogContent.find(".cmp-image__editor-link");
-            $linkURLField = $dialogContent.find('foundation-autocomplete[name="./linkURL"]');
-            $firstCtaLinkField = $dialogContent.find(firstCtaLinkFieldSelector);
+            $linkURLField = $linkURLGroup.find('foundation-autocomplete[name="./linkURL"]');
             captionTuple = new CheckboxTextfieldTuple(dialogContent, 'coral-checkbox[name="./titleValueFromDAM"]', 'input[name="./jcr:title"]');
             $cqFileUpload = $dialog.find(".cmp-image__editor-file-upload");
             $cqFileUploadEdit = $dialog.find(".cq-FileUpload-edit");
@@ -142,7 +141,7 @@
             }
             toggleAlternativeFieldsAndLink(imageFromPageImage, isDecorative);
             togglePageImageInherited(imageFromPageImage, isDecorative);
-
+            updateImageThumbnail();
         }
 
         $(window).adaptTo("foundation-registry").register("foundation.validation.selector", {
@@ -180,11 +179,6 @@
         updateImageThumbnail();
     });
 
-    // Update the image thumbnail when the checkbox to enable the call to action is toggled
-    $(document).on("change", dialogContentSelector + " coral-checkbox[name='./actionsEnabled']", function(e) {
-        updateImageThumbnail();
-    });
-
     $(document).on("change", dialogContentSelector + " " + presetTypeSelector, function(e) {
         switch (e.target.value) {
             case "imagePreset":
@@ -207,9 +201,9 @@
         var thumbnailConfigPath = $(dialogContentSelector).find(pageImageThumbnailSelector).attr(pageImageThumbnailConfigPathAttribute);
         var thumbnailComponentPath = $(dialogContentSelector).find(pageImageThumbnailSelector).attr(pageImageThumbnailComponentPathAttribute);
         $firstCtaLinkField = $dialogContent.find(firstCtaLinkFieldSelector);
-        if ($linkURLField && $linkURLField.adaptTo("foundation-field") && !$linkURLField.adaptTo("foundation-field").isDisabled()) {
+        if ($linkURLField && $linkURLField.adaptTo("foundation-field") && $linkURLField.adaptTo("foundation-field").getValue()) {
             linkValue = $linkURLField.adaptTo("foundation-field").getValue();
-        } else if ($firstCtaLinkField && $firstCtaLinkField.adaptTo("foundation-field") && !$firstCtaLinkField.adaptTo("foundation-field").isDisabled()) {
+        } else if ($firstCtaLinkField && $firstCtaLinkField.adaptTo("foundation-field") && $firstCtaLinkField.adaptTo("foundation-field").getValue()) {
             linkValue = $firstCtaLinkField.adaptTo("foundation-field").getValue();
         }
         if (linkValue === undefined || linkValue === "") {

bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/Utils.java
@@ -323,18 +323,22 @@ public class Utils {
 
         if (imageFromPageImage) {
             Resource inheritedResource = null;
-            Optional<Link> link = linkHandler.getLink(resource);
+            String linkURL = properties.get(ImageResource.PN_LINK_URL, String.class);
             boolean actionsEnabled = (currentStyle != null) ?
-                    !currentStyle.get(Teaser.PN_ACTIONS_DISABLED, !properties.get(Teaser.PN_ACTIONS_ENABLED, false)) :
-                    properties.get(Teaser.PN_ACTIONS_ENABLED, false);
-            if (actionsEnabled) {
+                    !currentStyle.get(Teaser.PN_ACTIONS_DISABLED, !properties.get(Teaser.PN_ACTIONS_ENABLED, true)) :
+                    properties.get(Teaser.PN_ACTIONS_ENABLED, true);
+            Resource firstAction = Optional.of(resource).map(res -> res.getChild(Teaser.NN_ACTIONS)).map(actions -> actions.getChildren().iterator().next()).orElse(null);
 
+            if (StringUtils.isNotEmpty(linkURL)) {
+                // the inherited resource is the featured image of the linked page
+                Optional<Link> link = linkHandler.getLink(resource);
+                inheritedResource = link
+                        .map(link1 -> (Page) link1.getReference())
+                        .map(ComponentUtils::getFeaturedImage)
+                        .orElse(null);
+            } else if (actionsEnabled && firstAction != null) {
                 // the inherited resource is the featured image of the first action's page (the resource is assumed to be a teaser)
-
-                inheritedResource = Optional.of(resource)
-                        .map(res -> res.getChild("actions"))
-                        .map(actions -> actions.getChildren().iterator().next())
-                        .map(firstAction -> linkHandler.getLink(firstAction, "link"))
+                inheritedResource = Optional.of(linkHandler.getLink(firstAction, Teaser.PN_ACTION_LINK))
                         .map(link1 -> {
                             if (link1.isPresent()) {
                                 Page linkedPage = (Page) link1.get().getReference();
@@ -345,18 +349,8 @@ public class Utils {
                             return null;
                         })
                         .orElse(null);
-            } else if (link.isPresent()) {
-
-                // the inherited resource is the featured image of the linked page
-
-                inheritedResource = link
-                        .map(link1 -> (Page) link1.getReference())
-                        .map(ComponentUtils::getFeaturedImage)
-                        .orElse(null);
             } else {
-
                 // the inherited resource is the featured image of the current page
-
                 inheritedResource = Optional.ofNullable(currentPage)
                         .map(page -> {
                             Template template = page.getTemplate();

bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/TeaserImpl.java
@@ -17,7 +17,9 @@ package com.adobe.cq.wcm.core.components.internal.models.v1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -100,7 +102,7 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
     /**
      * The target page.
      */
-    private Page targetPage;
+    protected Page targetPage;
 
     /**
      * The image src.
@@ -110,12 +112,12 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
     /**
      * Flag indicating if CTA actions are enabled.
      */
-    private boolean actionsEnabled = false;
+    protected boolean actionsEnabled = false;
 
     /**
      * Flag indicating if the title should be hidden.
      */
-    private boolean titleHidden = false;
+    protected boolean titleHidden = false;
 
     /**
      * Flag indicating if the title type should be hidden.
@@ -125,7 +127,7 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
     /**
      * Flag indicating if the description should be hidden.
      */
-    private boolean descriptionHidden = false;
+    protected boolean descriptionHidden = false;
 
     /**
      * Flag indicating if the image should not be linked.
@@ -145,12 +147,12 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
     /**
      * Flag indicating if the title should be inherited from the target page.
      */
-    private boolean titleFromPage = false;
+    protected boolean titleFromPage = false;
 
     /**
      * Flag indicating if the description should be inherited from the target page.
      */
-    private boolean descriptionFromPage = false;
+    protected boolean descriptionFromPage = false;
 
     /**
      * List of CTA actions.
@@ -165,6 +167,8 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
         add(JcrConstants.JCR_DESCRIPTION);
     }};
 
+    protected final Map<String, String> overriddenImageResourceProperties = new HashMap<>();
+
     /**
      * The teaser link
      */
@@ -172,7 +176,7 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
 
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     @Nullable
-    private String linkTarget;
+    protected String linkTarget;
 
     /**
      * The current component.
@@ -184,7 +188,7 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
      * The page manager.
      */
     @ScriptVariable
-    private PageManager pageManager;
+    protected PageManager pageManager;
 
     /**
      * The current style.
@@ -206,6 +210,15 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
      */
     @PostConstruct
     protected void initModel() {
+        initProperties();
+        initImage();
+        initLink();
+    }
+
+    /**
+     * Initialize the properties.
+     */
+    protected void initProperties() {
         ValueMap properties = resource.getValueMap();
 
         pretitleHidden = currentStyle.get(Teaser.PN_PRETITLE_HIDDEN, pretitleHidden);
@@ -222,10 +235,21 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
 
         titleFromPage = properties.get(Teaser.PN_TITLE_FROM_PAGE, titleFromPage);
         descriptionFromPage = properties.get(Teaser.PN_DESCRIPTION_FROM_PAGE, descriptionFromPage);
+    }
 
+    /**
+     * Initialize the image.
+     */
+    protected void initImage() {
         if (this.hasImage()) {
-            this.setImageResource(component, request.getResource(), hiddenImageResourceProperties, null);
+            this.setImageResource(component, request.getResource(), hiddenImageResourceProperties, overriddenImageResourceProperties);
         }
+    }
+
+    /**
+     * Initialize the link.
+     */
+    protected void initLink() {
         // use the target page as the link if it exists
         link = this.getTargetPage()
                 .map(page -> Optional.of(linkHandler.getLink(page.getPath(), linkTarget).orElse(null)))
@@ -293,7 +317,7 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
      * @return List of teaser actions.
      */
     @NotNull
-    private List<Action> getTeaserActions() {
+    protected List<Action> getTeaserActions() {
         if (this.actions == null) {
             this.actions = Optional.ofNullable(this.isActionsEnabled() ? this.resource.getChild(Teaser.NN_ACTIONS) : null)
                 .map(Resource::getChildren)
@@ -419,7 +443,7 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
     /**
      * Teaser CTA.
      */
-    @JsonIgnoreProperties({"path", "description", "lastModified", "name"})
+    @JsonIgnoreProperties({"path", "description", "lastModified", "name", "ctaPage"})
     public class Action extends AbstractListItemImpl implements ListItem {
 
         /**
@@ -482,7 +506,7 @@ public class TeaserImpl extends AbstractImageDelegatingModel implements Teaser {
          * @return The referenced page if this CTA references a page, empty if not.
          */
         @NotNull
-        protected Optional<Page> getCtaPage() {
+        public Optional<Page> getCtaPage() {
             return Optional.ofNullable(ctaLink.map(Link::getReference).orElse(null));
         }
 

bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v2/TeaserImpl.java
@@ -17,6 +17,9 @@ package com.adobe.cq.wcm.core.components.internal.models.v2;
 
 import java.util.Optional;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Exporter;
@@ -31,11 +34,13 @@ import com.adobe.cq.wcm.core.components.commons.link.Link;
 import com.adobe.cq.wcm.core.components.internal.Utils;
 import com.adobe.cq.wcm.core.components.models.Teaser;
 import com.day.cq.commons.DownloadResource;
+import com.day.cq.commons.ImageResource;
+import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.components.Component;
+import com.day.cq.wcm.foundation.Image;
+import com.day.text.Text;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.annotation.PostConstruct;
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {Teaser.class, ComponentExporter.class}, resourceType = TeaserImpl.RESOURCE_TYPE)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME , extensions = ExporterConstants.SLING_MODEL_EXTENSION)
@@ -43,24 +48,52 @@ public class TeaserImpl extends com.adobe.cq.wcm.core.components.internal.models
 
     public final static String RESOURCE_TYPE = "core/wcm/components/teaser/v2/teaser";
 
+    /**
+     * The title.
+     */
+    private String title;
+
+    /**
+     * The description.
+     */
+    private String description;
+
     @ScriptVariable
     protected Page currentPage;
 
-    /**
-     * The current component.
-     */
-    @ScriptVariable
-    private Component component;
+    @Override
+    protected void initProperties() {
+        titleFromPage = true;
+        descriptionFromPage = true;
+        actionsEnabled = true;
+        super.initProperties();
+    }
 
-    @PostConstruct
-    protected void initModel() {
-
-        super.initModel();
-
-        if (hasImage() && (super.isActionsEnabled() || (super.getTitle()!=null && !super.getTitle().isEmpty()))) {
-            super.hiddenImageResourceProperties.add(Link.PN_LINK_URL);
-            super.setImageResource(component, request.getResource(), super.hiddenImageResourceProperties, null);
+    @Override
+    protected void initImage() {
+        overriddenImageResourceProperties.put(Image.PN_LINK_URL, getTargetPage().map(Page::getPath).orElse(null));
+        overriddenImageResourceProperties.put(Teaser.PN_ACTIONS_ENABLED, Boolean.valueOf(actionsEnabled).toString());
+        if (StringUtils.isNotEmpty(getTitle()) || getTeaserActions().size() > 0) {
+            overriddenImageResourceProperties.put(Teaser.PN_IMAGE_LINK_HIDDEN, Boolean.TRUE.toString());
         }
+        super.initImage();
+    }
+
+    @Override
+    protected void initLink() {
+        // use the target page as the link if it exists
+        link = Optional.of(this.getTargetPage().map(page -> linkHandler.getLink(page.getPath(), linkTarget).orElse(null))
+                .orElse(
+                        Optional.of(linkHandler.getLink(resource, Link.PN_LINK_URL)
+                                .orElse(
+                                        Optional.ofNullable( actionsEnabled ? getActions().stream().findFirst().map(action -> linkHandler.getLink(action.getURL(), null)).orElse(null) : null)
+                                                .orElse(
+                                                        linkHandler.getLink(currentPage)
+                                                ).orElse(null)
+                                )
+                        ).orElse(null)
+                )
+        );
     }
 
     @Override
@@ -74,6 +107,60 @@ public class TeaserImpl extends com.adobe.cq.wcm.core.components.internal.models
     @Deprecated
     public String getLinkURL() {
         return super.getLinkURL();
+    }
+
+    @Override
+    @NotNull
+    protected Optional<Page> getTargetPage() {
+        if (this.targetPage == null) {
+            String linkURL = resource.getValueMap().get(ImageResource.PN_LINK_URL, String.class);
+            if (StringUtils.isNotEmpty(linkURL)) {
+                this.targetPage = Optional.ofNullable(this.resource.getValueMap().get(ImageResource.PN_LINK_URL, String.class))
+                        .map(this.pageManager::getPage).orElse(null);
+            } else if (actionsEnabled && getActions().size() > 0) {
+                this.targetPage = getTeaserActions().stream().findFirst()
+                        .flatMap(com.adobe.cq.wcm.core.components.internal.models.v1.TeaserImpl.Action::getCtaPage)
+                        .orElse(null);
+            } else {
+                targetPage = currentPage;
+            }
+        }
+        return Optional.ofNullable(this.targetPage);
+    }
+
+    @Override
+    public String getTitle() {
+        if (this.title == null && !this.titleHidden) {
+            if (titleFromPage) {
+                this.title = this.getTargetPage()
+                        .map(tp -> StringUtils.defaultIfEmpty(tp.getPageTitle(), tp.getTitle()))
+                        .orElseGet(() -> this.getTeaserActions().stream().findFirst()
+                                .map(com.adobe.cq.wcm.core.components.internal.models.v1.TeaserImpl.Action::getTitle)
+                                .orElseGet(() -> Optional.ofNullable(getCurrentPage())
+                                        .map(cp -> StringUtils.defaultIfEmpty(cp.getPageTitle(), cp.getTitle()))
+                                        .orElse(null)));
+            } else {
+                this.title = this.resource.getValueMap().get(JcrConstants.JCR_TITLE, String.class);
+            }
+        }
+        return title;
+    }
+
+    @Override
+    public String getDescription() {
+        if (this.description == null && !this.descriptionHidden) {
+            if (descriptionFromPage) {
+                this.description = this.getTargetPage().map(Optional::of).orElseGet(() -> Optional.ofNullable(getCurrentPage()))
+                        .map(Page::getDescription)
+                        // page properties uses a plain text field - which may contain special chars that need to be escaped in HTML
+                        // because the resulting description from the teaser is expected to be HTML produced by the RTE editor
+                        .map(Text::escapeXml)
+                        .orElse(null);
+            } else {
+                this.description = this.resource.getValueMap().get(JcrConstants.JCR_DESCRIPTION, String.class);
+            }
+        }
+        return this.description;
     }
 
     protected boolean hasImage() {

bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v3/ImageImpl.java
@@ -46,6 +46,7 @@ import com.day.cq.dam.api.Asset;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import static com.adobe.cq.wcm.core.components.internal.Utils.getWrappedImageResourceWithInheritance;
+import static com.adobe.cq.wcm.core.components.models.Teaser.PN_IMAGE_LINK_HIDDEN;
 
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {Image.class, ComponentExporter.class}, resourceType = ImageImpl.RESOURCE_TYPE)
@@ -56,18 +57,21 @@ public class ImageImpl extends com.adobe.cq.wcm.core.components.internal.models.
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ImageImpl.class);
 
+    private boolean imageLinkHidden = false;
+
     @PostConstruct
     protected void initModel() {
         super.initModel();
         if (hasContent) {
             disableLazyLoading = currentStyle.get(PN_DESIGN_LAZY_LOADING_ENABLED, false);
+            imageLinkHidden = properties.get(PN_IMAGE_LINK_HIDDEN, imageLinkHidden);
         }
     }
 
     @Override
     @Nullable
     public Link getImageLink() {
-        return link.orElse(null);
+        return imageLinkHidden ? null : link.orElse(null);
     }
 
     @Override

bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/models/v2/TeaserImplTest.java
@@ -31,6 +31,7 @@ import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 
 import static com.adobe.cq.wcm.core.components.internal.link.LinkTestUtils.assertValidLink;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -38,6 +39,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TeaserImplTest extends com.adobe.cq.wcm.core.components.internal.models.v1.TeaserImplTest {
 
     private static final String TEST_BASE = "/teaser/v2";
+    private static final String TEASER_25 = TEST_ROOT_PAGE + TEST_ROOT_PAGE_GRID + "/teaser-25";
 
     @BeforeEach
     protected void setUp() {
@@ -54,6 +56,15 @@ public class TeaserImplTest extends com.adobe.cq.wcm.core.components.internal.mo
         // < and > are expected escaped, because the page properties provide only a plain text field for the page description
         assertEquals("Teasers description from &lt;page properties&gt;", teaser.getDescription());
         Utils.testJSONExport(teaser, Utils.getTestExporterJSONPath(testBase, "teaser11"));
+    }
+
+    @Test
+    protected void testTeaserWithTitleAndDescriptionFromCurrentPage() {
+        Teaser teaser = getTeaserUnderTest(TEASER_25);
+        assertEquals("Teasers Test", teaser.getTitle());
+        // < and > are expected escaped, because the page properties provide only a plain text field for the page description
+        assertEquals("Teasers description from &lt;page properties&gt;", teaser.getDescription());
+        Utils.testJSONExport(teaser, Utils.getTestExporterJSONPath(testBase, "teaser25"));
     }
 
     @Test
@@ -76,12 +87,8 @@ public class TeaserImplTest extends com.adobe.cq.wcm.core.components.internal.mo
     @Override
     protected void testTeaserWithoutLink() {
         Teaser teaser = getTeaserUnderTest(TEASER_4);
-        // TODO: make the following assert pass
-        /*
-        assertThat(testLogger.getLoggingEvents(),
-                hasItem(debug("Teaser component from /content/teasers/jcr:content/root/responsivegrid/teaser-4 does not define a link.")));
-         */
-        assertNull(teaser.getLink(), "The link should be null");
+        assertNotNull(teaser.getLink(), "The link should not be null");
+        assertEquals("/core/content/teasers.html", teaser.getLink().getURL());
     }
 
     @Test
@@ -117,7 +124,8 @@ public class TeaserImplTest extends com.adobe.cq.wcm.core.components.internal.mo
     protected void testImageFromPage_withoutLink() {
         Teaser teaser = getTeaserUnderTest(TEASER_21);
         Link link = teaser.getLink();
-        assertNull(link, "link should be null");
+        assertNotNull(link);
+        assertEquals("/core/content/teasers.html", link.getURL());
         Utils.testJSONExport(teaser, Utils.getTestExporterJSONPath(testBase, "teaser21"));
     }
 

commit 94463956b6e52eb671c30c3ff1fc872d5acee687

Fix IT regressions after #1953 and #1976

content/src/content/jcr_root/apps/core/wcm/components/image/v2/image/clientlibs/editor/js/image.js
@@ -111,7 +111,7 @@
 
         $(window).adaptTo("foundation-registry").register("foundation.validation.selector", {
             submittable: ".cmp-image__editor-alt-text",
-            candidate: ".cmp-image__editor-alt-text",
+            candidate: ".cmp-image__editor-alt-text:not(:disabled)",
             exclusion: ".cmp-image__editor-alt-text *"
         });
     });

content/src/content/jcr_root/apps/core/wcm/components/image/v3/image/clientlibs/editor/js/image.js
@@ -75,7 +75,7 @@
             $altGroup = $dialogContent.find(".cmp-image__editor-alt");
             $altTextField = $dialogContent.find(".cmp-image__editor-alt-text");
             $linkURLGroup = $dialogContent.find(".cmp-image__editor-link");
-            $linkURLField = $linkURLGroup.find('foundation-autocomplete[name="./linkURL"]');
+            $linkURLField = $dialogContent.find('foundation-autocomplete[name="./linkURL"]');
             captionTuple = new CheckboxTextfieldTuple(dialogContent, 'coral-checkbox[name="./titleValueFromDAM"]', 'input[name="./jcr:title"]');
             $cqFileUpload = $dialog.find(".cmp-image__editor-file-upload");
             $cqFileUploadEdit = $dialog.find(".cq-FileUpload-edit");
@@ -146,7 +146,7 @@
 
         $(window).adaptTo("foundation-registry").register("foundation.validation.selector", {
             submittable: ".cmp-image__editor-alt-text",
-            candidate: ".cmp-image__editor-alt-text",
+            candidate: ".cmp-image__editor-alt-text:not(:disabled)",
             exclusion: ".cmp-image__editor-alt-text *"
         });
     });
@@ -286,8 +286,9 @@
             } else {
                 $linkURLGroup.show();
             }
-            if ($linkURLField.length) {
-                $linkURLField.adaptTo("foundation-field").setDisabled(isDecorativeCheckbox.checked);
+            var $imageLinkURLField = $linkURLGroup.find('foundation-autocomplete[name="./linkURL"]');
+            if ($imageLinkURLField.length) {
+                $imageLinkURLField.adaptTo("foundation-field").setDisabled(isDecorativeCheckbox.checked);
             }
             if ($altTextField.length) {
                 $altTextField.adaptTo("foundation-field").setRequired(!isDecorativeCheckbox.checked);