ff2b76daf7e569fa58e061af96d0db7be56e58a0
Vishal Singh Arya <vsarya95@gmail.com>
2022-04-18 16:44:12 +0530

    Table of Contents Component (#2030)
    * see requirements at https://github.com/adobe/aem-core-wcm-components/wiki/Table-of-Content-Component
    * adds unit tests and UI tests
    * adds a TOC page to the component library
    * Handles invalid start and stop levels
    
    Co-authored-by: Vishal Singh Arya <varya@adobe.com>
    Co-authored-by: Vlad <vlad@bailescu.ro>
    Co-authored-by: jckautzmann <jkautzma@adobe.com>

--- testing/it/e2e-selenium-utils/src/main/java/com/adobe/cq/wcm/core/components/it/seljup/util/Commons.java
+++ testing/it/e2e-selenium-utils/src/main/java/com/adobe/cq/wcm/core/components/it/seljup/util/Commons.java
@@ -143,6 +143,8 @@ public class Commons {
     // content fragment list component
     public static final String RT_CONTENTFRAGMENTLIST_V1 = "core-component/components/contentfragmentlist-v1";
     public static final String RT_CONTENTFRAGMENTLIST_V2 = "core-component/components/contentfragmentlist-v2";
+    // table of contents component
+    public static String RT_TABLEOFCONTENTS_V1 = "core-component/components/tableofcontents-v1";
     // core form container
     public static String RT_FORMCONTAINER_V1 = "core-component/components/formcontainer-v1";
     public static String RT_FORMCONTAINER_V2 = "core-component/components/formcontainer-v2";
@@ -704,9 +706,16 @@ public class Commons {
     /**
      * Save configuration for component
      */
-
     public static void saveConfigureDialog() throws InterruptedException {
         $(Selectors.SELECTOR_DONE_CONFIG_BUTTON).click();
+        webDriverWait(RequestConstants.WEBDRIVER_WAIT_TIME_MS);
+    }
+
+    /**
+     * Close configuration for component
+     */
+    public static void closeConfigureDialog() throws InterruptedException {
+        $(Selectors.SELECTOR_CANCEL_CONFIG_BUTTON).click();
         webDriverWait(RequestConstants.WEBDRIVER_WAIT_TIME_MS);
     }
 
@@ -720,7 +729,6 @@ public class Commons {
     /**
      * Check if panelselect is present
      */
-
     public static boolean isPanelSelectPresent() {
         return $(Selectors.SELECTOR_PANEL_SELECT).isDisplayed();
     }
