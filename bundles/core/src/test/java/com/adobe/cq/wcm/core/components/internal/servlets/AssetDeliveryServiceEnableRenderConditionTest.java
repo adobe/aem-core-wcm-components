eb4b6f6b8ede8e099cdcd0a9fa135bdbfc53b5fb
anagarwa <agarwal.ankit51@gmail.com>
2022-05-12 14:27:28 +0530

    SITES-4700

--- bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/AssetDeliveryServiceEnableRenderConditionTest.java
+++ bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/AssetDeliveryServiceEnableRenderConditionTest.java
@@ -16,32 +16,15 @@
 package com.adobe.cq.wcm.core.components.internal.servlets;
 
 import com.adobe.cq.wcm.core.components.context.CoreComponentTestContext;
-import com.adobe.cq.wcm.core.components.internal.models.v1.AbstractImageTest;
 import com.adobe.cq.wcm.spi.AssetDelivery;
-import com.adobe.granite.ui.components.ds.DataSource;
 import com.adobe.granite.ui.components.rendercondition.RenderCondition;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.SyntheticResource;
-import org.apache.sling.commons.metrics.Counter;
-import org.apache.sling.commons.metrics.MetricsService;
-import org.apache.sling.commons.metrics.Timer;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.osgi.framework.Constants;
-
-import java.util.Hashtable;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(AemContextExtension.class)
 public class AssetDeliveryServiceEnableRenderConditionTest {
