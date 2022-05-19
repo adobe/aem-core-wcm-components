102b5a4aae5a6038b247ce82d3275970a5f26f20
Burkhard Pauli <mail@bpauli.de>
2022-05-19 13:20:07 +0200

    AllowedColorSwatchesDataSourceServlet throws NPE when colorfield is used in a Multifield
    - optimize imports

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/servlets/AllowedColorSwatchesDataSourceServlet.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/servlets/AllowedColorSwatchesDataSourceServlet.java
@@ -20,10 +20,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 
