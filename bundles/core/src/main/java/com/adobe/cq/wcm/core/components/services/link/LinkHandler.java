46da5a1b5e0510bdd7e918f2a4578910b3f5af66
JC Kautzmann <jkautzma@adobe.com>
2022-05-23 11:27:38 +0200

    make link handler API public
    - draft API

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/services/link/LinkHandler.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/services/link/LinkHandler.java
@@ -21,8 +21,17 @@ import org.jetbrains.annotations.NotNull;
 
 import com.adobe.cq.wcm.core.components.commons.link.Link;
 
+/**
+ * Interface for resolving links.
+ */
 public interface LinkHandler {
 
+    /**
+     * Returns a link based on the configuration defined in the link builder.
+     *
+     * @param builder {@link LinkBuilder} holding the configuration for the link.
+     * @return {@link Optional} of  {@link Link}
+     */
     Optional<Link> getLink(@NotNull LinkBuilder builder);
 
 }
