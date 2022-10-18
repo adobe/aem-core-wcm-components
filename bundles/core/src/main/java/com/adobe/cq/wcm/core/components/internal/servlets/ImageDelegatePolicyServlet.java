ba078845e8a8c93f710b4d15524ecf2f6328ff8f
Burkhard Pauli <mail@bpauli.de>
2022-10-18 15:33:22 +0200

    Add DM support to Teaser component
    - provide servlet to include asset tab from image component into the the teaser dialog. In this case the cqDesign request attribute needs to be reset to the imageDelegate component
    - move the DM settings from the metadata tab to the asset tap in the image dialog, so it is still only required to include the asset tab within the teaser dialog
    
    relates to #1473

---
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/servlets/ImageDelegatePolicyServlet.java
@@ -0,0 +1,79 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2022 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.wcm.core.components.internal.servlets;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestPathInfo;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.SyntheticResource;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.jetbrains.annotations.NotNull;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.adobe.cq.wcm.core.components.internal.models.v1.AbstractImageDelegatingModel;
+import com.adobe.granite.ui.components.ExpressionResolver;
+import com.day.cq.wcm.api.WCMFilteringResourceWrapper;
+import com.day.cq.wcm.api.components.ComponentManager;
+
+@Component(service = { Servlet.class })
+@SlingServletResourceTypes(
+        resourceTypes = ImageDelegatePolicyServlet.RESOURCE_TYPE
+)
+public class ImageDelegatePolicyServlet extends SlingSafeMethodsServlet {
+
+    public static final String RESOURCE_TYPE = "core/wcm/components/include/imagedelegate";
+    private static final String PN_PATH = "path";
+
+    @Reference
+    ExpressionResolver expressionResolver;
+
+    @Override
+    protected void doGet(@NotNull SlingHttpServletRequest request, @NotNull SlingHttpServletResponse response)
+            throws ServletException, IOException {
+        ResourceResolver resourceResolver = request.getResourceResolver();
+        ComponentManager componentManager = resourceResolver.adaptTo(ComponentManager.class);
+        RequestPathInfo requestPathInfo = request.getRequestPathInfo();
+        com.day.cq.wcm.api.components.Component component =
+                Optional.ofNullable(requestPathInfo.getSuffix())
+                        .flatMap(s -> Optional.ofNullable(resourceResolver.getResource(s)))
+                        .flatMap(r -> Optional.ofNullable(componentManager)
+                                .map(c -> c.getComponentOfResource(r)))
+                        .orElse(null);
+        if (Objects.nonNull(component)) {
+            ValueMap properties = component.getProperties();
+            String imageDelegate = properties.get(AbstractImageDelegatingModel.IMAGE_DELEGATE, String.class);
+            RequestDispatcher requestDispatcher = Optional.ofNullable(request.getResource().getValueMap().get(PN_PATH
+                            , String.class))
+                    .map(p -> new WCMFilteringResourceWrapper(resourceResolver.getResource(p), new SyntheticResource(resourceResolver,
+                            requestPathInfo.getSuffix(),
+                            imageDelegate), expressionResolver, request)).map(request::getRequestDispatcher).orElse(null);
+            if (Objects.nonNull(requestDispatcher)) {
+                requestDispatcher.include(request, response);
+            }
+        }
+    }
+}
---
+++ bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/ImageDelegatePolicyServletTest.java
@@ -0,0 +1,71 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2022 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.wcm.core.components.internal.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+
+import org.apache.sling.testing.mock.sling.servlet.MockRequestDispatcherFactory;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.adobe.cq.wcm.core.components.context.CoreComponentTestContext;
+import com.adobe.granite.ui.components.ExpressionCustomizer;
+import com.adobe.granite.ui.components.ExpressionResolver;
+import com.day.cq.wcm.api.designer.Style;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith({AemContextExtension.class, MockitoExtension.class})
+class ImageDelegatePolicyServletTest {
+
+    private static final String TEST_BASE = "/image-delegate-policy-servlet";
+    private static final String APPS_ROOT = "/apps/core/wcm/components";
+    private static final String CONTENT_ROOT = "/content";
+    private static final String SUFFIX = "/content/test/jcr:content/root/responsivegrid/teaser";
+
+    public final AemContext context = CoreComponentTestContext.newAemContext();
+    private ImageDelegatePolicyServlet underTest;
+
+    @Mock
+    private MockRequestDispatcherFactory mockRequestDispatcherFactory;
+
+    @BeforeEach
+    void setUp() {
+        context.load().json(TEST_BASE + CoreComponentTestContext.TEST_CONTENT_JSON, CONTENT_ROOT);
+        context.load().json(TEST_BASE + CoreComponentTestContext.TEST_APPS_JSON, APPS_ROOT);
+        context.requestPathInfo().setSuffix(SUFFIX);
+        underTest = new ImageDelegatePolicyServlet();
+    }
+
+    @Test
+    void testCqDesignAttribute() throws ServletException, IOException {
+        context.currentResource("/apps/core/wcm/components/teaser/cq:design/content/items/tabs/items/image");
+        MockSlingHttpServletRequest request = context.request();
+        request.setRequestDispatcherFactory(mockRequestDispatcherFactory);
+        underTest.doGet(request, context.response());
+        ExpressionCustomizer expressionCustomizer = (ExpressionCustomizer) request.getAttribute(ExpressionCustomizer.class.getName());
+        assertNotNull(expressionCustomizer);
+        Style cqDesign = (Style) expressionCustomizer.getVariable("cqDesign");
+        assertNotNull(cqDesign);
+    }
+}
