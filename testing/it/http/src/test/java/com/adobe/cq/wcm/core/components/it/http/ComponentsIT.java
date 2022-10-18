38f474b3858452c4045fe8335aaff8405ffd44c2
JC Kautzmann <jkautzma@adobe.com>
2022-10-18 15:59:01 +0200

    SITES-8730: add integration tests to cover teaser corner cases

--- testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/ComponentsIT.java
+++ testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/ComponentsIT.java
@@ -17,6 +17,8 @@ package com.adobe.cq.wcm.core.components.it.http;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -29,23 +31,36 @@ import org.jsoup.nodes.TextNode;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.testing.client.CQClient;
 import com.adobe.cq.testing.junit.rules.CQAuthorClassRule;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ComponentsIT {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ComponentsIT.class);
+    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
     @ClassRule
     public static final CQAuthorClassRule cqBaseClassRule = new CQAuthorClassRule();
 
     static CQClient adminAuthor;
+    static String cp;
 
     @BeforeClass
     public static void beforeClass() {
         adminAuthor = cqBaseClassRule.authorRule.getAdminClient(CQClient.class);
+        // get the context path
+        cp = cqBaseClassRule.authorRule.getConfiguration().getUrl().getPath();
+        // configure the JSON_MAPPER to serialize json data attributes
+        JSON_MAPPER.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
     }
 
     @Test
@@ -79,6 +94,12 @@ public class ComponentsIT {
         return document;
     }
 
+    /**
+     * Removes noise from a given Node by traversing the structure and removing or processing attributes so that a reproducible output is
+     * generated.
+     *
+     * @param element
+     */
     private void removeNoise(Node element) {
         for (int i = 0; i < element.childNodeSize(); ) {
             Node child = element.childNode(i);
@@ -92,13 +113,37 @@ public class ComponentsIT {
                 child.remove();
                 continue;
             }
-            // normalize img src attributes
-            if (StringUtils.equals(child.nodeName(), "img") || StringUtils.equals(child.attr("data-cmp-is"), "image")) {
-                for (String attr : new String[] { "src", "data-cmp-src" }) {
-                    String src = child.attr(attr);
-                    if (StringUtils.isNotEmpty(src)) {
-                        src = src.replaceAll("/\\d+/", "/0/");
-                        child.attr(attr, src);
+            // remove timestamps from img src
+
+            for (String attr : new String[] { "src", "data-cmp-src" }) {
+                String value = child.attr(attr);
+                if (StringUtils.isNotEmpty(value)) {
+                    value = value.replaceAll("/\\d+/", "/0/");
+                    child.attr(attr, value);
+                }
+            }
+            // prepend context path if needed
+            if (StringUtils.isNotEmpty(cp)) {
+                for (String attr : new String[] { "src", "data-cmp-src", "href" }) {
+                    String value = child.attr(attr);
+                    if (StringUtils.startsWith(value, "/") && !StringUtils.startsWith(value, cp + '/')) {
+                        value = value.replaceAll("/\\d+/", "/0/");
+                        child.attr(attr, value);
+                    }
+                }
+            }
+            // normalize json data attributes
+            for (String attr : new String[] { "data-cmp-data-layer" }) {
+                String stringValue = child.attr(attr);
+                if (StringUtils.isNotEmpty(stringValue)) {
+                    try {
+                        JsonNode treeNode = JSON_MAPPER.readTree(stringValue);
+                        Map<String, Object> value = JSON_MAPPER.treeToValue(treeNode, Map.class);
+                        value = removeNoise(value);
+                        String processedJson = JSON_MAPPER.writeValueAsString(value);
+                        child.attr(attr, processedJson);
+                    } catch (IOException ex) {
+                        LOG.warn("Failed to normalize json attribute: {}", stringValue, ex);
                     }
                 }
             }
@@ -107,5 +152,41 @@ public class ComponentsIT {
             removeNoise(child);
             i++;
         }
+    }
+
+    /**
+     * Creates a deep copy of a Map parsed from a json object. It uses a {@link TreeMap} internally in order to produce a reproducible
+     * output. Some properties will be processed to remove noise to ensure the test outcome is reproducible.
+     *
+     * @param jsonMap
+     * @return
+     */
+    private Map<String, Object> removeNoise(Map<String, Object> jsonMap) {
+        // use a tree map as copy to enforce a natural ordering of the keys
+        Map<String, Object> copy = new TreeMap<>();
+
+        for (Map.Entry<String, ?> entry : jsonMap.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+
+            if (value instanceof Map) {
+                value = removeNoise((Map<String, Object>) value);
+            } else if (value instanceof String) {
+                // prepend context path if needed
+                if (StringUtils.isNotEmpty(cp)) {
+                    for (String attr : new String[] { "xdm:linkURL" }) {
+                        if (key.equals(attr)
+                            && StringUtils.startsWith((String) value, "/")
+                            && !StringUtils.startsWith((String) value, cp + '/')) {
+                            value = cp + value;
+                        }
+                    }
+                }
+            }
+
+            copy.put(key, value);
+        }
+
+        return copy;
     }
 }
