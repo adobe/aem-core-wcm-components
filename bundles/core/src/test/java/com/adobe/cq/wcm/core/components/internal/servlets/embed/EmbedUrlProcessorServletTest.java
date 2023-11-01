cd8c812468d4cf53cedd6d230625c1e30cc0eb24
Saairam Venkatesh <greenghost1100@gmail.com>
2023-10-31 21:33:00 -0500

    Expected all types of ordering for the json returned by pinterest api in EmbedUrlProcessorServletTest.testUrlWithRegisteredProvider

--- bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/embed/EmbedUrlProcessorServletTest.java
+++ bundles/core/src/test/java/com/adobe/cq/wcm/core/components/internal/servlets/embed/EmbedUrlProcessorServletTest.java
@@ -32,6 +32,7 @@ import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(AemContextExtension.class)
 public class EmbedUrlProcessorServletTest {
@@ -71,9 +72,11 @@ public class EmbedUrlProcessorServletTest {
     public void testUrlWithRegisteredProvider() throws Exception {
         context.request().setQueryString("url=https://www.pinterest.com/pin/99360735500167749/");
         servlet.doGet(context.request(), context.response());
-        String expectedOutput = "{\"processor\":\"pinterest\",\"options\":{\"pinId\":\"99360735500167749\"}}";
         assertEquals(HttpServletResponse.SC_OK, context.response().getStatus(), "Expected the 200 status code.");
         assertEquals("application/json;charset=utf-8", context.response().getContentType(), "Expected the JSON content type.");
-        assertEquals(expectedOutput, context.response().getOutputAsString(), "Does not match the expected response output.");
+        String expectedOutput1 = "{\"processor\":\"pinterest\",\"options\":{\"pinId\":\"99360735500167749\"}}";
+        String expectedOutput2 = "{\"options\":{\"pinId\":\"99360735500167749\"},\"processor\":\"pinterest\"}";
+        String actualOutput = context.response().getOutputAsString();
+        assertTrue(expectedOutput1.equals(actualOutput) || expectedOutput2.equals(actualOutput), "Does not match the expected response output.");
     }
 }
