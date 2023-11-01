40e76214aad449dc0b43b36c3660364f01f773bb
Saairam Venkatesh <greenghost1100@gmail.com>
2023-10-31 21:14:33 -0500

    Changed HashSet to LinkedHashSet in ClientLibrariesImpl

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/ClientLibrariesImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/ClientLibrariesImpl.java
@@ -297,7 +297,7 @@ public class ClientLibrariesImpl implements ClientLibraries {
     @NotNull
     protected Set<String> getCategoriesFromComponents() {
         try (ResourceResolver resourceResolver = resolverFactory.getServiceResourceResolver(Collections.singletonMap(ResourceResolverFactory.SUBSERVICE, COMPONENTS_SERVICE))) {
-            Set<String> categories = new HashSet<>();
+            Set<String> categories = new LinkedHashSet<>();
             for (ClientLibrary library : this.getAllClientLibraries(resourceResolver)) {
                 for (String category : library.getCategories()) {
                     if (pattern == null || pattern.matcher(category).matches()) {
