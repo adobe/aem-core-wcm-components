fc87990dfaf495e3de42790a23afe398f8417681
bcenusa <bcenusa@adobe.com>
2024-02-14 17:09:21 +0200

    SITES-19799: fix for nested panel containers

--- content/src/content/jcr_root/apps/core/wcm/components/commons/editor/clientlibs/panelcontainer/v1/js/PanelContainer.js
+++ content/src/content/jcr_root/apps/core/wcm/components/commons/editor/clientlibs/panelcontainer/v1/js/PanelContainer.js
@@ -102,7 +102,7 @@
             var that = this;
             var activeIndex = 0;
             if (that._config.el && that._config.panelContainerType) {
-                var items = $(this._config.el).find(that._config.panelContainerType.itemSelector);
+                var items = $(this._config.el).find("> * > " + that._config.panelContainerType.itemSelector);
                 items.each(function(index) {
                     var activeItemSelector = that._config.panelContainerType.itemActiveSelector;
                     if (that._config.el.id) {
