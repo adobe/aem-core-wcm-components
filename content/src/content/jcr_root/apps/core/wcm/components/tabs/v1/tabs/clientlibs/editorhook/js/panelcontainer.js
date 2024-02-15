c47af80309ce28da1a585b2c2974b55e3081da9b
bcenusa <bcenusa@adobe.com>
2024-02-15 16:33:21 +0200

    revert change

--- content/src/content/jcr_root/apps/core/wcm/components/tabs/v1/tabs/clientlibs/editorhook/js/panelcontainer.js
+++ content/src/content/jcr_root/apps/core/wcm/components/tabs/v1/tabs/clientlibs/editorhook/js/panelcontainer.js
@@ -20,7 +20,7 @@
         if (window.CQ && window.CQ.CoreComponents && window.CQ.CoreComponents.panelcontainer &&
             window.CQ.CoreComponents.panelcontainer.v1 && window.CQ.CoreComponents.panelcontainer.v1.registry) {
             window.CQ.CoreComponents.panelcontainer.v1.registry.register({
-                name: "sasds",
+                name: "cmp-tabs",
                 selector: ".cmp-tabs",
                 wrapperSelector: '[data-panelcontainer="tabs"]',
                 itemSelector: "[data-cmp-hook-tabs='tabpanel']",
