c1a65d41891561e2b1ca520858b564c5c8fe72a1
Egor_Cheptsov <egor.cheptsov@digitalum.eu>
2024-07-30 22:17:11 +0200

    [SITES-22335] remove imports

--- testing/it/e2e-selenium-utils/src/main/java/com/adobe/cq/wcm/core/components/it/seljup/util/components/image/ImageEditDialog.java
+++ testing/it/e2e-selenium-utils/src/main/java/com/adobe/cq/wcm/core/components/it/seljup/util/components/image/ImageEditDialog.java
@@ -26,9 +26,7 @@ import com.codeborne.selenide.DragAndDropOptions;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
