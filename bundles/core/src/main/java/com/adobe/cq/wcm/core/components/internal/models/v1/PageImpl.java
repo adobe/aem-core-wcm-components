57227fc37c4dc9cad113d9b1635b8c7dca80e72e
mumbler6 <benjaminguan001@gmail.com>
2024-11-10 17:50:04 -0600

    Fixed flakiness caused by HashSet ordering in PageImplTest

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/PageImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/PageImpl.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -149,6 +150,7 @@ public class PageImpl extends AbstractComponentImpl implements Page {
 
 	private String[] buildKeywords() {
 		Tag[] tags = currentPage.getTags();
+        Arrays.sort(tags, Comparator.comparing(Tag::getName));
         String[] keywords = new String[tags.length];
         int index = 0;
         Locale language= currentPage.getLanguage(false);
