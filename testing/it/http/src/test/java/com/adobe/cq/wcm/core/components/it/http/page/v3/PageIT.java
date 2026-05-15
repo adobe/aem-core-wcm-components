d6c75ba8bdd4dac87298eda82d9db6973a47cfe3
levente <levente@adobe.com>
2026-05-15 17:45:25 +0300

    SITES-42542 - Create a Core Component for Structured Data (JSON-LD) Injection
     * add minimal integration test verifying JSON-LD script tag is emitted in Page v3 head
    
     * gate test with @RunIfToggleEnabled("FT_SITES-42542") so it is skipped when the feature toggle is off
    
     * seed cq:structuredData on the existing simple-page-v3 test content

--- testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/page/v3/PageIT.java
+++ testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/page/v3/PageIT.java
@@ -28,6 +28,7 @@ import com.adobe.cq.testing.client.CQClient;
 import com.adobe.cq.testing.junit.assertion.GraniteAssert;
 import com.adobe.cq.testing.junit.rules.CQAuthorPublishClassRule;
 import com.adobe.cq.testing.junit.rules.CQRule;
+import com.adobe.cq.testing.junit.rules.toggles.RunIfToggleEnabled;
 
 public class PageIT {
 
@@ -76,6 +77,15 @@ public class PageIT {
         // async loading is enabled in the page policy
         String content = adminAuthor.doGet("/content/core-components/simple-page/simple-page-v3-clientlibs-async.html", 200).getContent();
         Pattern pattern = Pattern.compile("<script async src=\".*/etc.clientlibs/core/wcm/components/accordion/v1/accordion/clientlibs/site.*.js\"></script>");
+        GraniteAssert.assertRegExFind(message, content, pattern);
+    }
+
+    @Test
+    @RunIfToggleEnabled("FT_SITES-42542")
+    public void testStructuredData() throws ClientException {
+        String message = "The page head should contain the JSON-LD script tag for the cq:structuredData block";
+        String content = adminAuthor.doGet("/content/core-components/simple-page/simple-page-v3.html", 200).getContent();
+        Pattern pattern = Pattern.compile("<script type=\"application/ld\\+json\">\\{\"@context\":\"https://schema\\.org\",\"@type\":\"Organization\",\"name\":\"Acme\"}</script>");
         GraniteAssert.assertRegExFind(message, content, pattern);
     }
 
